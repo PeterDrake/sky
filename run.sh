0d46dceb26b35a4ecb3580a180ae5109659b9788
Max Levin <msl@lclark.edu>
2018-05-22 10:51:12 -0700

    Two 1-core trials, Two 32-core trials.

--- run.sh
+++ run.sh
@@ -4,9 +4,11 @@ echo $options
 # The %% * is a perverse trick to get first word of options, which is the job name
 #SGE_Batch -r "${options%% *}" -c "python3 -u preprocess.py $options" -P 1
 
-SGE_Batch -r "e53-00" -c "python3 -u train.py e53-00 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 1
-SGE_Batch -r "e53-01" -c "python3 -u train.py e53-01 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 2
-SGE_Batch -r "e53-02" -c "python3 -u train.py e53-02 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 4
-SGE_Batch -r "e53-03" -c "python3 -u train.py e53-03 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 8
-SGE_Batch -r "e53-04" -c "python3 -u train.py e53-04 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 16
-SGE_Batch -r "e53-05" -c "python3 -u train.py e53-05 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 32
+SGE_Batch -r "t1_cores_1" -c "python3 -u train.py e53-00 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 1
+SGE_Batch -r "t2_cores_1" -c "python3 -u train.py e53-00 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 1
+# SGE_Batch -r "e53-01" -c "python3 -u train.py e53-01 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 2
+# SGE_Batch -r "e53-02" -c "python3 -u train.py e53-02 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 4
+# SGE_Batch -r "e53-03" -c "python3 -u train.py e53-03 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 8
+# SGE_Batch -r "e53-04" -c "python3 -u train.py e53-04 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 16
+SGE_Batch -r "t1_cores_32" -c "python3 -u train.py e53-05 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 32
+SGE_Batch -r "t2_cores_32" -c "python3 -u train.py e53-05 a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-5-h" -P 32
