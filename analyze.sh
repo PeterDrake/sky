9ed555b1be91b7ef98dd59fc14ef026c7e997f08
Max Levin <msl@lclark.edu>
2018-05-25 15:24:51 -0700

    Script to launch analysis tasks on BLT

--- analyze.sh
+++ analyze.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {0..5}
+for i in {0..4}
 do
-    SGE_Batch -r "analysis_$i" -c "python3 -u analyze.py $1" -P 1
+    SGE_Batch -r "analysis_$i" -c "python3 -u analyze.py $1$i" -P 1
 done
