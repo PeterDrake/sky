593221920c3cb0520072375170c874044a9e1a1b
Max Levin <msl@lclark.edu>
2018-06-04 11:47:00 -0700

    Functions now have descriptive comments, code is more readable.

--- fsc.py
+++ fsc.py
@@ -10,11 +10,13 @@ from scipy import misc
 
 
 def get_mask(timestamp):
+	""" Returns the mask of a given timestamp in the simplemask data directory. Does not handle exceptions."""
 	return np.array(misc.imread('data/simplemask/simplemask' + str(timestamp) + '.png'))
 
 
-def show_skymask(timestamp, mask=None):
-	if not mask.any():
+def show_skymask(timestamp=None, mask=None):
+	""" Shows the mask for a given timestamp, alternatively can show a given mask."""
+	if mask is not None:
 		mask = get_mask(timestamp)
 	mask_image = Image.fromarray(mask.astype('uint8'))
 	mask_image.show()
@@ -22,20 +24,19 @@ def show_skymask(timestamp, mask=None):
 
 # numpy array (480, 480, 3) go through find first non black pixel
 def find_first_non_black_pixel(timestamp, mask=None):
-	if not mask.any():
+	""" Finds the location of the first non-black pixel in the mask, vertically."""
+	if mask is not None:
 		mask = get_mask(timestamp)
 	for i in range(mask.shape[0]):
 		for j in range(mask.shape[1]):
 			if tuple(mask[i, j]) != (0, 0, 0):
-				print(i, j)
-				mask[i, j] = [255, 0, 0]
 				return i, j
 
 
-def find_second_non_black_pixel(timestamp, mask=None):
-	if not mask.any():
+def find_last_non_black_pixel(timestamp, mask=None):
+	""" Finds the location of the last non-black pixel in the mask, vertically."""
+	if mask is not None:
 		mask = get_mask(timestamp)
-	print(mask[0])
 	for i in range(mask.shape[0]):
 		for j in range(mask.shape[1] - 1, -1, -1):
 			if tuple(mask[i, j]) != (0, 0, 0):
@@ -45,14 +46,17 @@ def find_second_non_black_pixel(timestamp, mask=None):
 
 
 def find_center(timestamp, mask=None):
+	""" Returns the center of the locations of the first and last non-black pixels and the difference in
+	height between them."""
 	p1 = find_first_non_black_pixel(timestamp, mask)
-	p2 = find_second_non_black_pixel(timestamp, mask)
+	p2 = find_last_non_black_pixel(timestamp, mask)
 	return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2), (p2[0] - p1[0]) / 2
 
 
-def get_pixels_in_center(timestamp, mask=None):
+def get_pixels_in_center(timestamp, mask=None, threshold=0.6):
+	""" Returns a new image mask containing pixels that are within a threshold of the center of the mask."""
 	center, r = find_center(timestamp, mask)
-	new_r = 0.6 * r
+	new_r = threshold * r
 	new_mask = mask.copy()
 	for i in range(mask.shape[0]):
 		for j in range(mask.shape[1]):
@@ -62,6 +66,7 @@ def get_pixels_in_center(timestamp, mask=None):
 
 
 def get_fsc(mask):
+	""" Computes the fractional sky cover from a given mask."""
 	sky_pixels = 0
 	cloud_pixels = 0
 	for i in range(mask.shape[0]):
@@ -95,8 +100,5 @@ def get_fsc(mask):
 if __name__ == '__main__':
 	timestamp = 20160414162830
 	mask = get_mask(timestamp)
-	# find_first_non_black_pixel(timestamp, mask)
-	# find_second_non_black_pixel(timestamp, mask)
 	show_skymask(timestamp, mask)
-	# print(find_center(timestamp,mask))
 	print(get_fsc(get_pixels_in_center(timestamp, mask)))
