fcf51faa0f9360b2c6dc9a76c53e7c1e0cc86e62
Zoe Harrington <zoeharrington@lclark.edu>
2018-06-04 15:09:34 -0700

    Changed to compute center of image using all cardinal directions instead of just two.

--- fsc.py
+++ fsc.py
@@ -11,46 +11,78 @@ from scipy import misc
 
 def get_mask(timestamp):
 	""" Returns the mask of a given timestamp in the simplemask data directory. Does not handle exceptions."""
-	return np.array(misc.imread('data/simplemask/simplemask' + str(timestamp) + '.png')) #TODO: Look into reading this as a uint8 image
+	return np.array(misc.imread(
+		'data/simplemask/simplemask' + str(timestamp) + '.png'))  # TODO: Look into reading this as a uint8 image
 
 
 def show_skymask(timestamp=None, mask=None):
 	""" Shows the mask for a given timestamp, alternatively can show a given mask."""
-	if mask is not None:
+	if mask is None:
 		mask = get_mask(timestamp)
 	mask_image = Image.fromarray(mask.astype('uint8'))
 	mask_image.show()
 
 
-def find_first_non_black_pixel(timestamp, mask=None):
-	""" Finds the location of the first non-black pixel in the mask, vertically."""
-	if mask is not None:
+def find_circle_boundary(timestamp, mask=None):
+	""" Finds the first non-black pixel in all cardinal directions."""
+	if mask is None:
 		mask = get_mask(timestamp)
+	answer = []
+	flag = False
+	# Top
 	for i in range(mask.shape[0]):
+		if flag:
+			break
 		for j in range(mask.shape[1]):
 			if tuple(mask[i, j]) != (0, 0, 0):
-				return i, j
+				answer += [i]
+				flag = True
+				break
 
-
-def find_last_non_black_pixel(timestamp, mask=None):
-	""" Finds the location of the last non-black pixel in the mask, vertically."""
-	if mask is not None:
-		mask = get_mask(timestamp)
+	# Bottom
+	flag = False
 	for i in range(mask.shape[0] - 1, -1, -1):
+		if flag:
+			break
 		for j in range(mask.shape[1]):
 			if tuple(mask[i, j]) != (0, 0, 0):
-				return mask.shape[0] - 1 - i, j #TODO: Check if this is off by 1 pixel
+				answer += [i]
+				flag = True
+				break
+	# Left
+	flag = False
+	for j in range(mask.shape[1]):
+		if flag:
+			break
+		for i in range(mask.shape[0]):
+			if tuple(mask[i, j]) != (0, 0, 0):
+				answer += [j]
+				flag = True
+				break
+	# Right
+	flag = False
+	for j in range(mask.shape[1] - 1, -1, -1):
+		if flag:
+			break
+		for i in range(mask.shape[0]):
+			if tuple(mask[i, j]) != (0, 0, 0):
+				answer += [j]
+				flag = True
+				break
+	return answer
 
 
 def find_center(timestamp, mask=None):
 	""" Returns the center of the locations of the first and last non-black pixels and the difference in
 	height between them."""
-	p1 = find_first_non_black_pixel(timestamp, mask)
-	p2 = find_last_non_black_pixel(timestamp, mask)
-	return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2), (p2[0] - p1[0]) / 2
+	t, b, l, r = find_circle_boundary(timestamp, mask)
+	r_vertical = (b - t) / 2
+	r_horizontal = (r - l) / 2
+	return ((t + b) / 2, (l + r) / 2), (r_vertical + r_horizontal) / 2
 
 
-def get_pixels_in_center(timestamp, mask=None, threshold=0.6):
+# TODO: Make this more efficient by not looping through all pixels & not duplicating mask
+def get_pixels_in_center(timestamp, mask=None, threshold=0.645):
 	""" Returns a new image mask containing pixels that are within a threshold of the center of the mask."""
 	center, r = find_center(timestamp, mask)
 	new_r = threshold * r
@@ -59,10 +91,9 @@ def get_pixels_in_center(timestamp, mask=None, threshold=0.6):
 		for j in range(mask.shape[1]):
 			if (i - center[0]) ** 2 + (j - center[1]) ** 2 > new_r ** 2:
 				new_mask[i, j] = [0, 0, 0]
-	return np.array(new_mask)
+	return new_mask
 
 
-# TODO: Make this more efficient by not looping through all pixels & not duplicating mask
 def get_fsc(mask):
 	""" Computes the fractional sky cover from a given mask. Returns total sky cover, opaque sky cover."""
 	sky_pixels = 0
@@ -85,26 +116,12 @@ def get_fsc(mask):
 	return (cloud_pixels + thin_pixels) / total, cloud_pixels / total
 
 
-# for index, x in np.ndenumerate(mask):
-#
-# #black = 3
-#
-# # top
-# for i in np.nditer():
-#     if
-#
-#         img = misc.imread('simplemask/' + file)
-#         b_mask[(img != color).any(axis=2)] = BLUE
-
-
-# preproccess.py simplify_all_masks to get all skymasks
-
 if __name__ == '__main__':
-	timestamp = 20160414162830
-	mask = get_mask(timestamp)
-	new_mask = get_pixels_in_center(timestamp, mask)
+	stamp = 20160414162830
+	mask = get_mask(stamp)
+	show_skymask(stamp, mask)
+	new_mask = get_pixels_in_center(stamp, mask)
+	show_skymask(stamp, new_mask)
 	fsc = get_fsc(new_mask)
-	show_skymask(mask=mask)
-	show_skymask(mask=new_mask)
+	print(find_center(stamp, mask))
 	print(fsc)
-
