36ab9b8d90a3b1f0e0401913f2dba35986fd6bbf
Zoe Harrington <zoeharrington@lclark.edu>
2018-06-13 16:01:32 -0700

    Fixed spelling

--- run_batch.py
+++ run_batch.py
@@ -1,13 +1,11 @@
 import sys
+
 from preprocess import simplify_image, simplify_mask
 
 if __name__ == "__main__":
 	f = open(sys.argv[1])  # This is the name of the file containing timestamps
 	print("Opened {}".format(sys.argv[1]))
-	for i in range(len(f)):
-		time = f[i]
+	for time in f:
 		time = time.replace('\n', '')
-		if i % 1000 == 0:
-			print("Simplifying image & mask number {}".format(i))
 		simplify_mask(time)
 		simplify_image(time)
