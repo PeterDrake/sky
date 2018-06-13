b0b899de7aeeaa5ea80edf6bf9cd1dc2e4594967
Zoe Harrington <zoeharrington@lclark.edu>
2018-06-13 15:55:09 -0700

    Corrected run_simplify.py to include -u

--- run_simplify.py
+++ run_simplify.py
@@ -2,5 +2,5 @@ import os
 
 files = os.listdir('res')
 for file in files:
-	os.system('SGE_batch -r "{}" -c "python3 run_batch.py {}" -P 1'.format(file[:-4], "res/" + file))
+	os.system('SGE_batch -r "{}" -c "python3 -u run_batch.py {}" -P 1'.format(file[:-4], "res/" + file))
 	print("Launched {} successfully".format(file))
