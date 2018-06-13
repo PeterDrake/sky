8515b17828f045d4ebac538ed5f259b116ecf8e6
Max Levin <msl@lclark.edu>
2018-06-13 14:17:55 -0700

    Added print statements for tracking progress of each batch.

--- run_batch.py
+++ run_batch.py
@@ -3,7 +3,11 @@ from preprocess import simplify_image, simplify_mask
 
 if __name__ == "__main__":
 	f = open(sys.argv[1])  # This is the name of the file containing timestamps
-	for time in f:
+	print("Opened {}".format(sys.argv[1]))
+	for i in range(len(f)):
+		time = f[i]
 		time = time.replace('\n', '')
+		if i % 1000 == 0:
+			print("Simplifying image & mask number {}".format(i))
 		simplify_mask(time)
 		simplify_image(time)
