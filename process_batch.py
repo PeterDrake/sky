eef62e1b4fc99cadb9c3e5540e793cffa7500d3e
Zoe Harrington <zoeharrington@lclark.edu>
2018-06-14 16:42:19 -0700

    made batch_process() a method

--- process_batch.py
+++ process_batch.py
@@ -4,12 +4,17 @@ one in each row.
 """
 
 import sys
+
 from preprocess import simplify_image, simplify_mask
 
-if __name__ == "__main__":
+
+def batch_process():
 	f = open(sys.argv[1])  # This is the name of the file containing timestamps
 	print("Opened {}".format(sys.argv[1]))
 	for time in f:
 		time = time.replace('\n', '')
 		simplify_mask(time)
 		simplify_image(time)
+
+if __name__ == "__main__":
+	batch_process()
