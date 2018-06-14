317ffce2f2be7952a8acc26a1a47bd50154596d5
Zoe Harrington <zoeharrington@lclark.edu>
2018-06-14 10:11:10 -0700

    Created compare_results.py to check if we create the correct amount of files

---
+++ compare_results.py
@@ -0,0 +1,28 @@
+from preprocess import *
+
+# Constants for input and output locations
+INPUT_DIR = '/home/users/jkleiss/TSI_C1'
+OUTPUT_DIR = '/home/users/msl/new_data'
+
+
+def count_expected():
+	good_times = extract_times_from_csv()
+	blacklist = find_unpaired_images(good_times, INPUT_DIR)
+	times = good_times - blacklist
+	print("This is the number of timestamps for which we should simplify: ", len(times))
+	return len(times)
+
+
+def count_actual():
+	print("This is the number of timestamps for which we simplify: ",
+	      len(extract_all_times(OUTPUT_DIR, ['/simpleimage', '/simplemask'])))
+	return len(extract_all_times(OUTPUT_DIR, ['/simpleimage', '/simplemask']))
+
+
+if __name__ == '__main__':
+	expected = count_expected()
+	actual = count_actual()
+	if expected == actual:
+		print('numbers match! Actual: ' + actual + 'Expected: ' + expected)
+	else .
+	print('numbers dont match. Actual: ' + actual + 'Expected: ' + expected)
