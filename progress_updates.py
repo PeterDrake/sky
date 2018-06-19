251dcc96abfc70a117a861ecbc632cae19f81a57
Max Levin <msl@lclark.edu>
2018-06-19 10:21:44 -0700

    Progress update script.

--- progress_updates.py
+++ progress_updates.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
 		for file in files:
 			with open(file, 'r') as f:
 				print(file)
-				print(f.readlines()[-1])
-				print()
+				print(f.readlines()[-1].strip('\n'))
+		print()
 		time.sleep(10)
