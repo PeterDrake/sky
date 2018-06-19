ec7e09c619496515323feade7123b8f70bf3244c
Max Levin <msl@lclark.edu>
2018-06-19 16:05:08 -0700

    Setup for debugging - network output

--- fsc.py
+++ fsc.py
@@ -161,7 +161,7 @@ def get_whole_fsc(mask):
 if __name__ == '__main__':
 	test_time = '20120501170000'
 	mask = get_simple_mask(test_time)
-	print(type(mask))
+	print(mask.shape)
 	mask = get_network_mask(test_time, 'e70-02')
-	print(type(mask))
+	print(mask.shape)
 	show_skymask(mask, save_instead=True, save_path='simplemasktest2.png')
