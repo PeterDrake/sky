af6b07d4f9c186998b7cd213640939200b09067a
Max Levin <msl@lclark.edu>
2018-06-19 10:18:34 -0700

    Progress update script.

---
+++ progress_updates.py
@@ -0,0 +1,20 @@
+"""
+Silly little file to print out updated progress for each training task running.
+You specify the directories, and it prints out their progress from output.txt.
+Just prints the last line from each directory.
+"""
+
+import time
+
+if __name__ == "__main__":
+	base_folder = "results/e70-"
+	sub_folders = ['00', '01', '02', '03', '04']
+	files = []
+	for ext in sub_folders:
+		files.append(base_folder + ext + "/output.txt")
+
+	while True:
+		for file in files:
+			with open(file, 'r') as f:
+				print(f.readlines()[-1])
+		time.sleep(10)
