c9b3a88f41720283ce940128a4020196f174159f
Max Levin <msl@lclark.edu>
2018-06-20 15:41:41 -0700

    Removed usage of pandas, since it did not run on Mayo.

--- launch_fsc.py
+++ launch_fsc.py
@@ -6,7 +6,10 @@ exp_labels = ['e70-00', 'e70-01', 'e70-02', 'e70-03', 'e70-04']
 num_batches = len(exp_labels)
 num_batches_per_network = 1
 
-total_length = len(extract_data_from_csv('shcu_good_data.csv', 'timestamp_utc'))
+total_length = -1  # This file has a header
+for line in open('shcu_good_data.csv'):
+	total_length += 1
+
 batch_length = total_length / num_batches_per_network
 
 for exp_label in exp_labels:
