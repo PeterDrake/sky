a009a7fc991c811d6f73a278482dc67f20d5aaa4
Max Levin <msl@lclark.edu>
2018-06-20 15:48:07 -0700

    Renamed job id and made batch_length an integer

--- launch_fsc.py
+++ launch_fsc.py
@@ -9,11 +9,11 @@ total_length = -1  # This file has a header
 for line in open('shcu_good_data.csv'):
 	total_length += 1
 
-batch_length = total_length / num_batches_per_network
+batch_length = int(total_length / num_batches_per_network)
 
 for exp_label in exp_labels:
 	for i in range(num_batches_per_network):
-		name = exp_label + "-" + str(i)
+		name = "batchnet-" + exp_label + "-" + str(i)
 		start = batch_length * i
 		finish = batch_length * (i + 1) if batch_length * (i + 1) < total_length else total_length
 		os.system('SGE_Batch -r "{}" -c "python3 -u fsc.py {} {} {}" -P 1'.format(name, exp_label, start, finish))
