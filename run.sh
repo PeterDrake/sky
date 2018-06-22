34b8e0bfbb92ee8198ad3507513e90d3092c16f9
Max Levin <msl@lclark.edu>
2018-06-22 16:47:08 -0700

    Changed cores to be 25 so that no two training jobs can run on BLT at the same time. This prevents tensorflow from competing with itself for resources.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo $options
 # The %% * is a perverse trick to get first word of options, which is the job name
-SGE_Batch -r "${options%% *}" -c "python3 -u train.py $options" -P 2
+SGE_Batch -r "${options%% *}" -c "python3 -u train.py $options" -P 25
