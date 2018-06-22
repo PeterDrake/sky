e22a86b4521e7ae16d85144ae88a7d457308d5c4
Max Levin <msl@lclark.edu>
2018-06-22 11:22:03 -0700

    Debugging.

--- fsc.py
+++ fsc.py
@@ -114,11 +114,11 @@ if __name__ == '__main__':
 	times = sorted(list(extract_data_from_csv('shcu_good_data.csv', 'timestamp_utc')))
 	with open('results/' + exp_label + '/' + 'fsc.csv', 'w') as f:
 		f.write("timestamp_utc, fsc_z, fsc_thn_z, fsc_opq_z" + "\n")
-	count = 0
-	for t in times:
-		if count % 100 == 0:
-			print("progress: ", count / len(times))
-		if os.path.isfile(extract_network_mask_path_from_time(t, exp_label)):
-			fsc_z, fsc_thn_z, fsc_opq_z = get_fsc_from_file(extract_network_mask_path_from_time(t, exp_label))
-			f.write("{}, {}, {}, {}".format(t, fsc_z, fsc_thn_z, fsc_opq_z) + "\n")
-		count += 1
+		count = 0
+		for t in times:
+			if count % 100 == 0:
+				print("progress: ", count / len(times))
+			if os.path.isfile(extract_network_mask_path_from_time(t, exp_label)):
+				fsc_z, fsc_thn_z, fsc_opq_z = get_fsc_from_file(extract_network_mask_path_from_time(t, exp_label))
+				f.write("{}, {}, {}, {}".format(t, fsc_z, fsc_thn_z, fsc_opq_z) + "\n")
+			count += 1
