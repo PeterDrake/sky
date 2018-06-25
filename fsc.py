8a76a7ec61da8fdeb9b4b085aa252fafd6121b75
Max Levin <msl@lclark.edu>
2018-06-25 15:19:39 -0700

    Removed spaces because apparently spaces screw everything up.

--- fsc.py
+++ fsc.py
@@ -113,7 +113,7 @@ if __name__ == '__main__':
 	exp_label = sys.argv[1]  # The experiment number / directory name in results
 	times = sorted(list(extract_data_from_csv('shcu_good_data.csv', 'timestamp_utc')))
 	with open('results/' + exp_label + '/' + 'fsc.csv', 'w') as f:
-		f.write("timestamp_utc, fsc_z, fsc_thn_z, fsc_opq_z" + "\n")
+		f.write("timestamp_utc,fsc_z,fsc_thn_z,fsc_opq_z" + "\n")
 		count = 0
 		for t in times:
 			if count % 10 == 0:
@@ -121,5 +121,5 @@ if __name__ == '__main__':
 				f.flush()
 			if os.path.isfile(extract_network_mask_path_from_time(t, exp_label)):
 				fsc_z, fsc_thn_z, fsc_opq_z = get_fsc_from_file(extract_network_mask_path_from_time(t, exp_label))
-				f.write("{}, {}, {}, {}".format(t, fsc_z, fsc_thn_z, fsc_opq_z) + "\n")
+				f.write("{},{},{},{}".format(t, fsc_z, fsc_thn_z, fsc_opq_z) + "\n")
 			count += 1
