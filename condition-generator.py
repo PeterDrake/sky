c374ec148e6518a26f8cc5cd280a2deb3c35e6a3
Zoe Harrington <zoeharrington@lclark.edu>
2018-06-25 09:51:47 -0700

    Merge branch 'compare_fsc' of /Users/student/PycharmProjects/sky with conflicts.

--- condition-generator.py
+++ condition-generator.py
@@ -13,11 +13,10 @@ exp = 72
 i = 0
 variants = ('a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-{1}-e g:conv-{0}-{1}-f h:concat-g-in i:conv-{0}-4-h',)
 
-
 with open("conditions.txt", 'w') as conditions:
-	for v in variants:
+    for v in variants:
 		for kernel_width in (5,):
-			for channels in (25,):
-				for j in range(3):
-					conditions.write('e{}-{:0>2} '.format(exp, i) + v.format(kernel_width, channels) + "\n")
-					i += 1
+		    for channels in (25,):
+			    for j in range(3):
+				    conditions.write('e{}-{:0>2} '.format(exp, i) + v.format(kernel_width, channels) + "\n")
+		            i += 1
