68ccc43644dd86543246e1b9e730fd3e6f6c1c03
Max Levin <msl@lclark.edu>
2018-06-25 15:59:05 -0700

    Debugging for types.

--- utils.py
+++ utils.py
@@ -123,7 +123,9 @@ def extract_fsc_for_date_from_dataframe(frame, timestamp):
 	# for h in df.columns.values:
 	# 	print('<{}>'.format(h))
 	ans = df.loc[timestamp, "fsc_z"]
-	print(ans)
+	print(type(ans))
+	print(type(ans.item()))
+	print(type(np.asscalar(ans)))
 	return (math.floor(ans * 10 ** 6)) / 10 ** 6
 
 
