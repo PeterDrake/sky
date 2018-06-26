e4207fd5fe38b6cc75ca0717638eba2f4020d1b7
Zoe Harrington <zoeharrington@lclark.edu>
2018-06-26 15:26:51 -0700

    test show_plot_of_pixel_difference

--- analyze.py
+++ analyze.py
@@ -97,6 +97,7 @@ def show_plot_of_pixel_difference(timestamps, exp_label, directory):
 		# tsi_mask = np.array(misc.imread(extract_mask_path_from_time(t, 'good_data/simplemask/' + time_to_year(t) + '/' + time_to_month_and_day(t) + "/simplemask" + t + ".png")))
 		t = str(t)
 		print(t)
+		print([t])
 		# if os.path.isfile(extract_network_mask_path_from_time(timestamp, exp_label)) and os.path.isfile(
 		# extract_mask_path_from_time(t, 'good_data')):
 		# print("I was here")
