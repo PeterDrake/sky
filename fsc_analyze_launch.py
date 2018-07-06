92441ef4aabeef9aef75940a67d40c42af862328
Zoe Harrington <zoeharrington@lclark.edu>
2018-07-06 14:12:20 -0700

    update docstring for fsc_analyze_launch.py

--- fsc_analyze_launch.py
+++ fsc_analyze_launch.py
@@ -1,3 +1,9 @@
+"""
+Run this program to launch fsc_analyze.py
+Creates and saves plots comparing fsc value from ceilometer, network and TSI for good and bad data (for timestamps
+from shcu_good_data.csv or shcu_bad_data.csv)
+"""
+
 import os
 
 exp_labels = ['e70-00', 'e70-01', 'e70-02', 'e70-03',
