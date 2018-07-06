d20067b8fa3a371be4cc5605b3b28b6b828600b4
Max Levin <msl@lclark.edu>
2018-07-06 13:57:27 -0700

    Plots NETWORK vs TSI Fractional Sky Cover now. Fixed mislabelled axes. Plots look gorgeous.

--- fsc_analyze.py
+++ fsc_analyze.py
@@ -27,7 +27,8 @@ def find_worst_results(filename, num_worst=5):
 	return sorted(disagreement_rates)
 
 
-def extract_arscl_and_image_fsc_from_dataframes(arscl_dataframe, image_dataframe):
+def extract_arscl_and_image_fsc_from_dataframes(arscl_dataframe, image_dataframe, arscl_header="cf_tot",
+		image_header="fsc_z"):
 	"""Returns lists containing fractional sky cover obtained from two dataframes. Expects 'image_dataframe' to be a
 	pandas dataframe with a header for 'fsc_z' and expects 'arscl_dataframe' to be a pandas dataframe with a header
 	for 'cf_tot'. Expects both dataframes to have a header for 'timestamp_utc'. Additionally expects the dataframes to
@@ -37,20 +38,19 @@ def extract_arscl_and_image_fsc_from_dataframes(arscl_dataframe, image_dataframe
 	arscl_times = set(extract_data_from_dataframe(arscl_dataframe, "timestamp_utc"))
 	times = image_times.intersection(arscl_times)  # Necessary to correct for missing times
 	x, y = [], []
-	sd = 0
+	mse = 0
 	for t in times:
-		x.append(extract_data_for_date_from_dataframe("fsc_z", t, image_dataframe))
-		y.append(extract_data_for_date_from_dataframe("cf_tot", t, arscl_dataframe))
-		sd += abs(y[-1] - x[-1])
-	return x, y, (sd / len(times)) ** 0.5
+		x.append(extract_data_for_date_from_dataframe(arscl_header, t, arscl_dataframe))
+		y.append(extract_data_for_date_from_dataframe(image_header, t, image_dataframe))
+		mse += (y[-1] - x[-1]) ** 2
+	return x, y, (mse / len(times)) ** 0.5
 
 
 if __name__ == "__main__":
-	N_SAMPLES = 4000
+	N_SAMPLES = 4500
 	exp_labels = ['e70-00']
 
 	for exp_label in exp_labels:
-
 		# Reads data from shcu_good_data.csv, takes a sample of the times, and gets data for plotting
 		good_arscl_dataframe = read_csv_file('shcu_good_data.csv')  # Contains both ARSCL and TSI Data
 		good_arscl_dataframe = good_arscl_dataframe.dropna(subset=['fsc_z', 'cf_tot', 'timestamp_utc'])
@@ -77,20 +77,41 @@ if __name__ == "__main__":
 		bad_network_dataframe = bad_network_dataframe[bad_network_dataframe['timestamp_utc'].isin(bad_times)]
 		bad_arscl_network = extract_arscl_and_image_fsc_from_dataframes(bad_arscl_dataframe, bad_network_dataframe)
 
+		# Gets comparison data for TSI and Network decision images on good and bad data
+		good_tsi_network = extract_arscl_and_image_fsc_from_dataframes(good_arscl_dataframe, good_network_dataframe, arscl_header="fsc_z")
+		bad_tsi_network = extract_arscl_and_image_fsc_from_dataframes(bad_arscl_dataframe, bad_network_dataframe, arscl_header="fsc_z")
+
+		# Makes four plots for the performance comparison and prints out the Root Mean Squared Error
 		x_label = 'CF SHCU'
 		y_labels = ['TSI FSC'] * 2 + ['NETWORK FSC'] * 2
 		titles = ['Good Data', 'Bad Data', 'Good Data', 'Bad Data']
 		DATA = [good_arscl_tsi, bad_arscl_tsi, good_arscl_network, bad_arscl_network]
-		fig = plt.figure()
+		fig = plt.figure(figsize=(12, 8))
 		for i, data in enumerate(DATA):
 			ax = fig.add_subplot(2, 2, i + 1)
 			ax.set_ylabel(y_labels[i])
 			ax.set_xlabel(x_label)
-			ax.set_title(titles[i])
+			ax.set_title(titles[i] + " ({})".format(i + 1))
 			plt.plot([0, 1], [0, 1], c='orange', lw=2)
 			plt.scatter(data[0], data[1], s=.25)
 		plt.tight_layout()
-		plt.savefig("results/" + exp_label + "/good_tsi_arscl_fsc.png", dpi=600)
-
+		plt.savefig("results/" + exp_label + "/good_tsi_arscl_fsc.png", dpi=300)
 		for i, data in enumerate(DATA):
-			print("The standard deviation for plot {} is {}.".format(i+1, data[2]))
+			print("The RMSE for plot {} is {}.".format(i + 1, data[2]))
+
+		# Makes two plots for direct comparison between TSI and Network on good and bad data
+		fig2 = plt.figure(figsize=(12, 8))
+		titles = ["Good Data", "Bad Data"]
+		DATA = [good_tsi_network, bad_tsi_network]
+		for i, title in enumerate(titles):
+			ax = fig2.add_subplot(1, 2, i + 1)
+			ax.set_xlabel("TSI FSC")
+			ax.set_ylabel("NETWORK FSC")
+			ax.set_title(title)
+			plt.scatter(DATA[i][0], DATA[i][1], s=0.25)
+			plt.plot([0, 1], [0, 1], c='orange', lw=2)
+		plt.tight_layout()
+		plt.savefig("results/" + exp_label + "/compare_tsi_network_fsc.png", dpi=300)
+		print("The RMSE for TSI/NET on GOOD DATA is {}.".format(good_tsi_network[2]))
+		print("The RMSE for TSI/NET on BAD DATA is {}.".format(bad_tsi_network[2]))
+
