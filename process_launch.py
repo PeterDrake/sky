e2a75876fa5bdba15ed39cc61001b170d671581a
Max Levin <msl@lclark.edu>
2018-07-09 10:56:07 -0700

    Setup for processing sky photos for experiment 73.

--- process_launch.py
+++ process_launch.py
@@ -7,25 +7,25 @@ of batches times the number of batches per network is small enough so that BLT c
 
 import os
 
-INPUT_DIR = "bad_data"
-input_csv = "shcu_bad_data.csv"
+INPUT_DIR = "good_data"
+input_csv = "shcu_good_data.csv"
 
 # Specify the labels that correspond to networks of interest. Ie 'e70-00'
-exp_labels = ['e70-00', 'e70-01', 'e70-02', 'e70-03', 'e70-04']
+exp_labels = ['e73-00', 'e73-01']
 
 if __name__ == "__main__":
 	num_batches = len(exp_labels)
 	num_batches_per_network = 5
 
 	total_length = -1  # This file has a header
-	for line in open('shcu_bad_data.csv'):
+	for line in open(input_csv):
 		total_length += 1
 
 	batch_length = int(total_length / num_batches_per_network)
 
 	for exp_label in exp_labels:
 		for i in range(num_batches_per_network):
-			name = "bad-net-" + exp_label + "-" + str(i)
+			name = "good-net-" + exp_label + "-" + str(i)
 			start = batch_length * i
 			finish = batch_length * (i + 1) if batch_length * (i + 1) < total_length else total_length
 			os.system('SGE_Batch -r "{}" -c "python3 -u process.py {} {} {}" -P 1'.format(name, exp_label, int(start),
