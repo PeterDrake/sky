494c274f77fee16f136d438720ad2124c16723ae
Max Levin <msl@lclark.edu>
2018-07-11 10:58:21 -0700

    Creates pickled python lists of timestamps in the 'bad_data' directory on BLT.

--- poster_stamps_launch.py
+++ poster_stamps_launch.py
@@ -3,23 +3,25 @@ This script can be launched at any time.
 
 Grabs "timestamp_utc" data from a given csv file and separates it into two chunks for later use. One chunk is for
 validation/standard plotting, the other is for testing/final plotting. Saves the two chunks into VALID_FILE and
-TEST_FILE as pickled python lists.
+TEST_FILE in the OUTPUT_DIR directory as pickled python lists.
 """
 
 import pickle
 from utils import extract_data_from_csv
 
-# Set the csv file containing the timestamps to set
+# Set the csv file containing the timestamps to use.
 INPUT_DATA_CSV = 'shcu_bad_data.csv'
 
+# Set the directory to save the timestamps in.
+OUTPUT_DIR = 'bad_data'
+
 # Set the filenames to store the batches in
-VALID_FILE = 'poster_valid.stamps'
-TEST_FILE = 'poster_test.stamps'
+VALID_FILE = OUTPUT_DIR + '/poster_valid.stamps'
+TEST_FILE = OUTPUT_DIR + '/poster_test.stamps'
 
 # Set the ratio of validation to total stamps. Ie: RATIO > 0.5 means more validation than testing.
 RATIO = 0.6
 
-# TODO: Shuffle the times before writing it to a list.
 if __name__ == "__main__":
 	times = extract_data_from_csv(INPUT_DATA_CSV, "timestamp_utc")
 	timestamps = list(times)
