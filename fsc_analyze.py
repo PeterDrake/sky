6dd5d543ae0d37cf64dea88d0d499ba990726c0f
Maxwell <msl@lclark.edu>
2018-09-21 19:12:54 -0700

    Allow plots to be created on the BLT cluster instead of locally.

--- fsc_analyze.py
+++ fsc_analyze.py
@@ -97,8 +97,8 @@ def scatter_plot(title, xlabel, ylabel, scatter, name):
 
 if __name__ == "__main__":
 	N_SAMPLES = 2500
-	exp_label = sys.argv[1].decode('utf-8')
-	# exp_label = 'e76-00'
+	# exp_label = sys.argv[1].decode('utf-8')
+	exp_label = 'e76-00'
 
 	# Reads data from shcu_good_data.csv, takes a sample of the times, and gets data for plotting
 	good_arscl_dataframe = read_csv_file('shcu_good_data.csv')  # Contains both ARSCL and TSI Data
