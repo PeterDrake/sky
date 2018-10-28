b97933ea27afe2175d160d5d5513e8dfdc34c136
Maxwell <msl@lclark.edu>
2018-10-27 22:57:30 -0700

    Setup processing for the two e78 networks.

--- process_launch.py
+++ process_launch.py
@@ -10,16 +10,16 @@ of batches times the number of batches per network is small enough so that BLT c
 import os
 
 # Specify the directory where the sky images are stored: ex: good_data
-INPUT_DIR = "bad_data"
+INPUT_DIR = "good_data"
 
 # Specify the location of the csv file that contains desired timestamp_utc information.
-INPUT_DATA_CSV = "bad_data/shcu_bad_data.csv"
+INPUT_DATA_CSV = "good_data/shcu_good_data.csv"
 
 # Specify the base of the job id
-JOB_NAME = "bad-net-"
+JOB_NAME = "process-good-"
 
 # Specify the labels that correspond to networks of interest. Ie 'e70-00'
-exp_labels = ['e77-00']
+exp_labels = ['e78-00', 'e78-01']
 
 # Specify the number of batches to run per network. This helps parallelize the processing task.
 num_batches_per_network = 5
