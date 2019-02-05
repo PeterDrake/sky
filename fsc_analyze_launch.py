b64bc236c728673c556e05a5b53ef22905edfac8
Zoe Harrington <zoeharrington@lclark.edu>
2019-02-05 15:50:04 -0800

    new branch for renaming data to typical and dubious. Started changing fsc_analyze_launch.py

--- fsc_analyze_launch.py
+++ fsc_analyze_launch.py
@@ -4,12 +4,12 @@ the Laser Ceilometer.
 
 This script can be run after fsc_launch.py has been run and its tasks have finished.
 
-This script requires four files: 'shcu_good_data.csv', 'shcu_bad_data.csv', 'good_fsc.csv', and 'bad_fsc.csv'.
-* The 'shcu' files should be located in the good_data/ and bad_data/ directories.
-* The 'fsc' files should be located in the results/EXP_LABEL/ directory. EX: results/e70-00/good_fsc.csv
+This script requires four files: 'shcu_typical_data.csv', 'shcu_dubious_data.csv', 'typical_fsc.csv', and 'dubious_fsc.csv'.
+* The 'shcu' files should be located in the typical_data/ and dubious_data/ directories.
+* The 'fsc' files should be located in the results/EXP_LABEL/ directory. EX: results/e70-00/typical_fsc.csv
 Both of these requirements should be satisfied if the previous scripts have been run correctly.
 
-This script creates two plots and saves them as 'good_tsi_arscl_fsc.png' and 'compare_tsi_network_fsc.png' in the
+This script creates two plots and saves them as 'typical_tsi_arscl_fsc.png' and 'compare_tsi_network_fsc.png' in the
 results/EXP_LABEL/ directory.
 """
 
@@ -21,8 +21,8 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 #from fsc_launch import INPUT_DATA_CSV
-# from poster_stamps_launch import DUBIOUS_VALID_FILE  # TODO: Make this a bit more clear - bad valid stamps file
-# from preprocess_stamps_launch import TYPICAL_VALID_FILE  # TODO: Make this a bit more clear - good valid stamps file
+# from poster_stamps_launch import DUBIOUS_VALID_FILE  # TODO: Make this a bit more clear - dubious valid stamps file
+# from preprocess_stamps_launch import TYPICAL_VALID_FILE  # TODO: Make this a bit more clear - typical valid stamps file
 from utils import read_csv_file, extract_data_from_dataframe, extract_data_for_date_from_dataframe
 
 N_SAMPLES = 2500
@@ -99,13 +99,13 @@ def scatter_plot(scatter, name, ylabel, title, xlabel):
 
 if __name__ == "__main__":
 
-	# Reads data from shcu_good_data.csv, takes a sample of the times, and gets data for plotting
-	good_arscl_dataframe = read_csv_file('good_data/shcu_good_data.csv')  # Contains both ARSCL and TSI Data
-	good_arscl_dataframe = good_arscl_dataframe.dropna(subset=['fsc_z', 'cf_tot', 'timestamp_utc'])
-	good_times = load_pickled_file(TYPICAL_VALID_FILE)
-	good_times = good_times[0:N_SAMPLES]
-	good_arscl_dataframe = good_arscl_dataframe[good_arscl_dataframe['timestamp_utc'].isin(good_times)]
-	good_arscl_tsi = extract_arscl_and_image_fsc_from_dataframes(good_arscl_dataframe, good_arscl_dataframe)
+	# Reads data from shcu_typical_data.csv, takes a sample of the times, and gets data for plotting
+	typical_arscl_dataframe = read_csv_file('typical_data/shcu_typical_data.csv')  # Contains both ARSCL and TSI Data
+	typical_arscl_dataframe = typical_arscl_dataframe.dropna(subset=['fsc_z', 'cf_tot', 'timestamp_utc'])
+	typical_times = load_pickled_file(TYPICAL_VALID_FILE)
+	typical_times = typical_times[0:N_SAMPLES]
+	typical_arscl_dataframe = typical_arscl_dataframe[typical_arscl_dataframe['timestamp_utc'].isin(typical_times)]
+	typical_arscl_tsi = extract_arscl_and_image_fsc_from_dataframes(typical_arscl_dataframe, typical_arscl_dataframe)
 
 	# Reads data from shcu_bad_data.csv, takes a sample of the times, and gets data for plotting
 	bad_arscl_dataframe = read_csv_file('bad_data/shcu_bad_data.csv')  # Contains both ARSCL and TSI Data
@@ -115,11 +115,11 @@ if __name__ == "__main__":
 	bad_arscl_dataframe = bad_arscl_dataframe[bad_arscl_dataframe['timestamp_utc'].isin(bad_times)]
 	bad_arscl_tsi = extract_arscl_and_image_fsc_from_dataframes(bad_arscl_dataframe, bad_arscl_dataframe)
 
-	# Reads data from good_fsc.csv and uses the times sample from shcu_good_data.csv to get data for plotting
-	good_network_dataframe = read_csv_file('results/' + EXP_LABEL + '/typical_fsc.csv')  # Contains NETWORK Data
-	good_network_dataframe = good_network_dataframe.dropna(subset=['fsc_z', 'timestamp_utc'])
-	good_network_dataframe = good_network_dataframe[good_network_dataframe['timestamp_utc'].isin(good_times)]
-	good_arscl_network = extract_arscl_and_image_fsc_from_dataframes(good_arscl_dataframe, good_network_dataframe)
+	# Reads data from typical_fsc.csv and uses the times sample from shcu_typical_data.csv to get data for plotting
+	typical_network_dataframe = read_csv_file('results/' + EXP_LABEL + '/typical_fsc.csv')  # Contains NETWORK Data
+	typical_network_dataframe = typical_network_dataframe.dropna(subset=['fsc_z', 'timestamp_utc'])
+	typical_network_dataframe = typical_network_dataframe[typical_network_dataframe['timestamp_utc'].isin(typical_times)]
+	typical_arscl_network = extract_arscl_and_image_fsc_from_dataframes(typical_arscl_dataframe, typical_network_dataframe)
 
 	# Reads data from bad_fsc.csv and uses the times sample from shcu_bad_data.csv to get data for plotting
 	bad_network_dataframe = read_csv_file('results/' + EXP_LABEL + '/dubious_fsc.csv')  # Contains NETWORK Data
@@ -127,17 +127,17 @@ if __name__ == "__main__":
 	bad_network_dataframe = bad_network_dataframe[bad_network_dataframe['timestamp_utc'].isin(bad_times)]
 	bad_arscl_network = extract_arscl_and_image_fsc_from_dataframes(bad_arscl_dataframe, bad_network_dataframe)
 
-	# Gets comparison data for TSI and Network decision images on good and bad data
-	good_tsi_network = extract_arscl_and_image_fsc_from_dataframes(good_arscl_dataframe, good_network_dataframe,
+	# Gets comparison data for TSI and Network decision images on typical and bad data
+	typical_tsi_network = extract_arscl_and_image_fsc_from_dataframes(typical_arscl_dataframe, typical_network_dataframe,
 	                                                               arscl_header="fsc_z")
 	bad_tsi_network = extract_arscl_and_image_fsc_from_dataframes(bad_arscl_dataframe, bad_network_dataframe,
 	                                                              arscl_header="fsc_z")
 
-	# Good/Bad Data FSC vs CF Plots for TSI and Network Decision Images
-	titles = ['Good Data', 'Bad Data']
+	# typical/Bad Data FSC vs CF Plots for TSI and Network Decision Images
+	titles = ['typical Data', 'dubious Data']
 	ylabels = ['FSC (TSI)', 'FSC (Network)']
 	xlabels = ['Ceilometer CF'] * 2
-	data = [good_arscl_tsi, bad_arscl_tsi, good_arscl_network, bad_arscl_network]
+	data = [typical_arscl_tsi, dubious_arscl_tsi, typical_arscl_network, dubious_arscl_network]
 	fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(14, 9), sharey=True, sharex=True)
 	for ax, col in zip(axes[0], titles):
 		ax.set_title(col, fontsize=30)
@@ -156,8 +156,8 @@ if __name__ == "__main__":
 	# RMSE plot
 	# data to plot
 	n_groups = 2
-	tsi_rsme = (good_arscl_tsi[2], bad_arscl_tsi[2])
-	network_rsme = (good_arscl_network[2], bad_arscl_network[2])
+	tsi_rsme = (typical_arscl_tsi[2], dubious_arscl_tsi[2])
+	network_rsme = (typical_arscl_network[2], dubious_arscl_network[2])
 	fig = plt.figure(figsize=(12, 9))
 	index = np.arange(n_groups)
 	bar_width = 0.3
@@ -174,6 +174,6 @@ if __name__ == "__main__":
 		bottom=False,  # ticks along the bottom edge are off
 		top=False,  # ticks along the top edge are off
 		labelbottom=True)
-	plt.xticks((index + bar_width / 2), ('Good Data', 'Bad Data'), fontsize=26)
+	plt.xticks((index + bar_width / 2), ('typical Data', 'dubious Data'), fontsize=26)
 	ax.legend(fontsize=20)
 	fig.savefig("results/" + EXP_LABEL + "/fsc_rmse_barchart.png")
