de32f7b7481e6048d390a316a6ae32f2d1117e4e
Maxwell <msl@lclark.edu>
2019-03-03 12:29:01 -0800

    Now uses parameters defined in config.py. Also removed older code and improved formatting.

--- preprocess_setup_launch.py
+++ preprocess_setup_launch.py
@@ -1,9 +1,9 @@
 """
 Preprocess Total Sky Imager data from arm.gov. To use this:
 
-1) Get the SkyImage and CloudMask data from ARM and unpack tars into your INPUT_DIR
+1) Get the SkyImage and CloudMask data from ARM and unpack tars into your RAW_DATA_DIR
 2) Specify the OUTPUT_DIR (which need not already exist). We set this to 'typical_data' and 'dubious_data'
-3) Specify BATCH_SIZE to help parallelize this process. This is the number of timestamps each batch needs to process.
+3) Specify PREPROCESS_BATCH_SIZE to help parallelize this process. This is the number of timestamps each batch needs to process.
 To run sequentially instead of in parallel, set this absurdly high.
 4) Run this program, wait for it to finish.
 5) Run preprocess_launch.py to do the actual simplification of masks & cropping of images
@@ -18,22 +18,9 @@ been cropped and had the sunband removed.
 See the code at the end for a high-level description of the steps this program goes through.
 """
 
+from config import *
 from random import shuffle
 from utils import *
-
-# Constants for input and output locations
-# INPUT_DIR = '/home/users/jkleiss/TSI_C1'
-INPUT_DIR = 'D:\Documents - Hard Drive\Lewis & Clark\Research\Sky ML\TSI_C1'
-#OUTPUT_DIR = 'typical_data'
-#TIMESTAMP_DATA_CSV = 'typical_data/shcu_typical_data.csv'  # shcu_dubious_data has about 5,000 times, shcu_typical_data has about 100,000 times
-
-# Used to create batches of timestamps. This is the number of images to preprocess in a single job.
-# We suggest using 1000 for dubious_data, and 5000 to 10000 for typical_data if using a cluster
-BATCH_SIZE = 1000
-
-
-# Option to specify a small number for a simple run through the model. Set to None for actual preprocessing
-small_process_size = 100
 
 
 def create_dirs(timestamps, output_dir, res_dir):
@@ -55,10 +42,9 @@ def create_dirs(timestamps, output_dir, res_dir):
 		for mmdd in seen[year]:
 			os.makedirs(output_dir + "/simpleimage/" + year + "/" + mmdd, exist_ok=True)
 			os.makedirs(output_dir + "/simplemask/" + year + "/" + mmdd, exist_ok=True)
-	return
 
 
-def make_batches_by_size(timestamps, batch_size=BATCH_SIZE):
+def make_batches_by_size(timestamps, batch_size=PREPROCESS_BATCH_SIZE):
 	"""Returns a set of batches of timestamps. The number of batches is determined by the length of the timestamps
 	collection and the number of elements in a batch can be provided by the user. batch_size is set to 10000 by
 	default as a best guess at the trade-off between parallelization and practicality."""
@@ -72,34 +58,34 @@ def make_batches_by_size(timestamps, batch_size=BATCH_SIZE):
 	return time_batches
 
 
-def setup(OUTPUT_DIR, TIMESTAMP_DATA_CSV):
-	RES_DIR = OUTPUT_DIR + '/res'
+def setup(output_dir, timestamp_data_csv):
+	res_dir = output_dir + '/res'
 	print("Cleaning the csv file.")
-	clean_csv(TIMESTAMP_DATA_CSV)
+	clean_csv(timestamp_data_csv)
 	print("Reading times from typical csv file.")
-	typical_times = extract_data_from_csv(TIMESTAMP_DATA_CSV, "timestamp_utc")
+	typical_times = extract_data_from_csv(timestamp_data_csv, "timestamp_utc")
 	print("Finished reading times. Eliminating unpaired times.")
-	blacklist = find_unpaired_images(typical_times, INPUT_DIR)
+	blacklist = find_unpaired_images(typical_times, RAW_DATA_DIR)
 	times = typical_times - blacklist
 	print("{} paired images and masks found.".format(len(times)))
 	# This can be used to process a small amount images instead of everything specified by the csv files.
-	if small_process_size:
+	if SMALL_PROCESS_SIZE:
 		few_times = set()
 		count = 0
 		for t in times:
 			few_times.add(t)
 			count += 1
-			if count == 100:
+			if count == SMALL_PROCESS_SIZE:
 				break
 		times = few_times
 		print("Using the first {} paired images and masks.".format(len(times)))
 	print("Creating directories for results.")
-	create_dirs(times, OUTPUT_DIR, RES_DIR)
+	create_dirs(times, output_dir, res_dir)
 	print("Directories created. Preparing batches.")
 	batches = make_batches_by_size(times)
 	print("Batches prepared. Writing batches to file.")
 	for i in range(len(batches)):
-		name = RES_DIR + "/batch" + str(i) + ".txt"
+		name = res_dir + "/batch" + str(i) + ".txt"
 		if not os.path.isfile(name):  # The batch file should not already exist.
 			f = open(name, 'w')
 			print("Writing batch {} data to {}".format(i, name))
@@ -113,5 +99,5 @@ def setup(OUTPUT_DIR, TIMESTAMP_DATA_CSV):
 
 
 if __name__ == '__main__':
-	setup('typical_data','typical_data/shcu_typical_data.csv')
-	setup('dubious_data', 'dubious_data/shcu_dubious_data.csv')
+	setup(TYPICAL_DATA_DIR, TYPICAL_DATA_CSV)
+	setup(DUBIOUS_DATA_DIR, DUBIOUS_DATA_CSV)
