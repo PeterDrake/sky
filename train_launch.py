b14faf2f070e98312426931367db76725a16e5d2
Maxwell <msl@lclark.edu>
2019-03-03 21:41:49 -0800

    Uses network structure from config file

--- train_launch.py
+++ train_launch.py
@@ -16,12 +16,10 @@ import os
 from config import *
 from train import train
 
-# Specify the structure of the network. This defines how train.py constructs the network to train.
-net = 'a:conv-3-32-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-3-32-e g:conv-3-32-f h:concat-g-in i:conv-3-4-h'
 
 if __name__ == "__main__":
 	if BLT:
-		os.system('SGE_Batch -r "{}" -c "python3 -u train.py {}" -P {}'.format(EXPERIMENT_LABEL, net, JOB_PRIORITY))
+		os.system('SGE_Batch -r "{}" -c "python3 -u train.py {}" -P {}'.format(EXPERIMENT_LABEL, NETWORK_STRUCTURE, JOB_PRIORITY))
 	else:
-		train(net)
+		train(NETWORK_STRUCTURE)
 
