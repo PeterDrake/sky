97401678f27c92eccd248b1e5cdcaacf2993b037
Maxwell <msl@lclark.edu>
2019-03-03 21:41:15 -0800

    Moved creation of stamps for dubious data into preprocess_stamps_launch (from poster_stamps_launch)

--- preprocess_stamps_launch.py
+++ preprocess_stamps_launch.py
@@ -10,10 +10,11 @@ SGE_Batch -r "preprocess_stamps" -c "python3 -u preprocess_stamps_launch.py" -P 
 
 import numpy as np
 import os
+import pickle
 from PIL import Image
 from scipy import misc
-from utils import extract_times_from_files_in_directory, separate_data, BLACK, BLUE
-from config import TYPICAL_DATA_DIR, TYPICAL_DATA_CSV
+from utils import extract_data_from_csv, extract_times_from_files_in_directory, separate_data, BLACK, BLUE
+from config import TYPICAL_DATA_DIR, DUBIOUS_DATA_DIR, DUBIOUS_DATA_CSV
 
 
 def create_constant_mask(color, filename):
@@ -33,7 +34,25 @@ def setup(train, valid, test):
 
 
 if __name__ == "__main__":
+	# For typical data
+	print("Creating training, validation, and test stamps for typical data")
 	train_stamp_path = TYPICAL_DATA_DIR + '/train.stamps'
 	valid_stamp_path = TYPICAL_DATA_DIR + '/valid.stamps'
 	test_stamp_path = TYPICAL_DATA_DIR + '/test.stamps'
 	setup(train_stamp_path, valid_stamp_path, test_stamp_path)
+	print("Finished. Stamps saved at: ", train_stamp_path, ", ", valid_stamp_path, ", ", test_stamp_path)
+
+	# For dubious data
+	print("Creating validation and test stamps for dubious data")
+	valid_stamp_path = DUBIOUS_DATA_DIR + '/poster_valid.stamps'  # TODO: Rename these to valid and test .stamps
+	test_stamp_path = DUBIOUS_DATA_DIR + '/poster_test.stamps'
+	ratio_valid_test = 0.6  # The ratio of validation to testing data for dubious data.
+	timestamps = extract_data_from_csv(DUBIOUS_DATA_CSV, "timestamp_utc")
+	timestamps = list(timestamps)
+	valid_stamps = timestamps[:int(len(timestamps) * ratio_valid_test)]
+	test_stamps = timestamps[int(len(timestamps) * ratio_valid_test):]
+	with open(valid_stamp_path, 'wb') as f:
+		pickle.dump(valid_stamps, f)
+	with open(test_stamp_path, 'wb') as f:
+		pickle.dump(test_stamps, f)
+	print("Finished. Stamps saved at: ", valid_stamp_path, ", ", test_stamp_path)
