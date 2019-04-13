19777cfd6b91a545dee86987083cbda2f1fd1b81
Maxwell <msl@lclark.edu>
2019-04-12 22:17:41 -0700

    Joins dataframes for network, tsi, and arscl measurements of fsc and cf.

--- make_agreement_plots.py
+++ make_agreement_plots.py
@@ -15,7 +15,6 @@
 	those values on the right side of the plot.
 """
 
-
 from config import *
 import pandas as pd
 
@@ -24,13 +23,34 @@ import pandas as pd
 # RESULTS_DIR/EXPERIMENT_LABEL/dubious_fsc.csv
 # Note: This obviously requires the network to be trained and for it to have processed a number of sky images. In
 # addition, fsc processing needs to be done (by running fsc_launch.py after process_launch.py finishes).
-typical_network_fsc_file = pd.read_csv(RESULTS_DIR + "/" + EXPERIMENT_LABEL + "/typical_fsc.csv")
-dubious_network_fsc_file = pd.read_csv(RESULTS_DIR + "/" + EXPERIMENT_LABEL + "/dubious_fsc.csv")
+typical_network_fsc_df = pd.read_csv(RESULTS_DIR + "/" + EXPERIMENT_LABEL + "/typical_fsc.csv")
+dubious_network_fsc_df = pd.read_csv(RESULTS_DIR + "/" + EXPERIMENT_LABEL + "/dubious_fsc.csv")
 
 # The tsi fsc and arscl cloud fraction values can be obtained from the csv files:
 # shcu_typical_data.csv
 # shcu_dubious_data.csv
 # Note: The paths to these files are already defined in config.py. Additionally, note that the header for TSI fsc in the
 # zenith region is 'fsc_z' and the header for arscl cloud fraction is 'cf_tot'.
-typical_arscl_fsc_cf_file = pd.read_csv(TYPICAL_DATA_CSV)
-dubious_arscl_fsc_cf_file = pd.read_csv(DUBIOUS_DATA_CSV)
+typical_arscl_fsc_cf_df = pd.read_csv(TYPICAL_DATA_CSV)
+dubious_arscl_fsc_cf_df = pd.read_csv(DUBIOUS_DATA_CSV)
+
+# Drops useless columns in typical_network_fsc_df and renames 'fsc_z' to 'net_fsc_z' to prevent overloaded column names
+# in a later join. Drops useless columns in typical_arscl_fsc_cf_df, then joins typical_network_fsc_df and
+# typical_arscl_fsc_cf_df on 'timestamp_utc'.
+typical_network_fsc_df = typical_network_fsc_df.filter(['timestamp_utc', 'fsc_z'])
+typical_network_fsc_df = typical_network_fsc_df.rename(columns={'fsc_z': 'net_fsc_z'})
+typical_arscl_fsc_cf_df = typical_arscl_fsc_cf_df.filter(['timestamp_utc', 'fsc_z', 'cf_tot'])
+typical_data_df = typical_network_fsc_df.join(typical_arscl_fsc_cf_df.set_index('timestamp_utc'), on='timestamp_utc')
+typical_data_df = typical_data_df.dropna()  # Drop rows with missing values from mismatched timestamps
+
+# Drops useless columns in dubious_network_fsc_df and renames 'fsc_z' to 'net_fsc_z' to prevent overloaded column names
+# in a later join. Drops useless columns in dubious_arscl_fsc_cf_df, then joins dubious_network_fsc_df and
+# dubious_arscl_fsc_cf_df on 'timestamp_utc'.
+dubious_network_fsc_df = dubious_network_fsc_df.filter(['timestamp_utc', 'fsc_z'])
+dubious_network_fsc_df = dubious_network_fsc_df.rename(columns={'fsc_z': 'net_fsc_z'})
+dubious_arscl_fsc_cf_df = dubious_arscl_fsc_cf_df.filter(['timestamp_utc', 'fsc_z', 'cf_tot'])
+dubious_data_df = dubious_network_fsc_df.join(dubious_arscl_fsc_cf_df.set_index('timestamp_utc'), on='timestamp_utc')
+dubious_data_df = dubious_data_df.dropna()  # Drop rows with missing values from mismatched timestamps
+
+print(typical_data_df.head())
+print(dubious_data_df.head())
