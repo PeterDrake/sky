55583d87d3f869dc7b13acdaeb9e2330e4ff4383
Leon Rogge <leonrogge@lclark.edu>
2019-05-28 11:28:52 -0700

    Added more test to test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -9,6 +9,24 @@ class TestUtil(unittest.TestCase):
 	def test_extracts_year_from_time(self):
 		self.assertEqual('2017', time_to_year('20170814092430'))
 
+	def test_extracts_month_from_time(self):
+		self.assertEqual('08', time_to_month('20170814092430'))
+
+	def test_extracts_day_from_time(self):
+		self.assertEqual('14', time_to_day('20170814092430'))
+
+	def test_extracts_month_and_day_from_time(self):
+		self.assertEqual('0814', time_to_month_and_day('20170814092430'))
+
+	def test_extracts_year_month_day_from_time(self):
+		self.assertEqual('20170814', time_to_year_month_day('20170814092430'))
+
+	def test_extracts_hour_minute_second_from_time(self):
+		self.assertEqual('092430', time_to_hour_minute_second('20170814092430'))
+
+	def test_extracts_(self):
+		self.assertEqual('092430', extract_timestamp('20160415235930.png'))
+
 
 if __name__ == '__main__':
 	unittest.main()
