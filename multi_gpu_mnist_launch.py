59117b840ce38376677326858feb520efbc84cdb
Leon Rogge <leonrogge@lclark.edu>
2019-06-04 16:12:25 -0700

    Trying to set up multi_gpu usage. Submitting 3 jobs at the same time to see if they get queued. ATTEMPT #13

--- multi_gpu_mnist_launch.py
+++ multi_gpu_mnist_launch.py
@@ -2,5 +2,5 @@ import os
 from config import *
 
 if __name__ == "__main__":
-	for i in range(3):
+	for i in range(10):
 		os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 multi_gpu_mnist.py" -P {}'.format('multi-gpu-results-' + str(i), JOB_PRIORITY))
