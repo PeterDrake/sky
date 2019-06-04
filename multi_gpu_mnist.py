d6e658263a765368347f15db3e590ef8add9630f
Leon Rogge <leonrogge@lclark.edu>
2019-06-04 14:33:58 -0700

    Trying to set up multi_gpu usage. ATTEMPT #4

--- multi_gpu_mnist.py
+++ multi_gpu_mnist.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
-
+from keras.utils import multi_gpu_model
 
 if __name__ == '__main__':
+
     mnist = tf.keras.datasets.mnist
 
     (x_train, y_train),(x_test, y_test) = mnist.load_data()
@@ -18,5 +19,6 @@ if __name__ == '__main__':
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=5)
-    model.evaluate(x_test, y_test)
+	parallel_model = multi_gpu_model(model, gpus=None)
+    parallel_model.fit(x_train, y_train, epochs=5)
+    parallel_model.evaluate(x_test, y_test)
