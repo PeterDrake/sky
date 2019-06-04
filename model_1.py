21982302c79647249a6c9a4348deb4aab1e90255
acschall <schall@lclark.edu>
2019-06-04 10:37:28 -0700

    Updated names, cleaned up max pool layers.

--- model_1.py
+++ model_1.py
@@ -12,50 +12,46 @@ np.random.seed(123)  # for reproducibility
 b_mask = color_mask(np.asarray(imageio.imread(TYPICAL_DATA_DIR + '/always_black_mask.png')), index_of(BLACK, COLORS))
 
 
-def max_pool(prev, width, height):
-    return tf.nn.max_pool(prev, [1, width, height, 1], strides=[1, 1, 1, 1], padding='SAME')
+sky_images = Input(shape=(480, 480, 3), name='SkyImages')
+first_conv = Convolution2D(filters=32, kernel_size=3, padding='same', data_format='channels_last', activation='relu', name='FirstConvolution')(sky_images)
 
-
-first_input = Input(shape=(480, 480, 3), name='Input')
-first_conv = Convolution2D(filters=32, kernel_size=3, padding='same', data_format='channels_last', activation='relu', name='FirstConvolution')(first_input)
-
-first_maxpool = Lambda(lambda x: max_pool(first_conv, 1, 100), name='FirstMaxPool')(first_conv)
-second_maxpool = Lambda(lambda x: max_pool(first_conv, 100, 1), name='SecondMaxPool')(first_conv)
+first_maxpool = Lambda(lambda x: tf.nn.max_pool(first_conv, [1, 1, 100, 1], strides=[1, 1, 1, 1], padding='SAME'), name='FirstMaxPool')(first_conv)
+second_maxpool = Lambda(lambda x: tf.nn.max_pool(first_conv, [1, 100, 1, 1], strides=[1, 1, 1, 1], padding='SAME'), name='SecondMaxPool')(first_conv)
 
 merge_one = concatenate([first_conv, first_maxpool], axis=3)
 merge_two = concatenate([second_maxpool, merge_one], axis=3)
 
 second_conv = Convolution2D(filters=32, kernel_size=3, padding='same', data_format='channels_last', activation='relu', name='SecondConvolution')(merge_two)
 
-merge_three = concatenate([second_conv, first_input], axis=3)
+merge_three = concatenate([second_conv, sky_images], axis=3)
 
 third_conv = Convolution2D(filters=4, kernel_size=3, padding='same', data_format='channels_last', activation='relu', name='ThirdConvolution')(merge_three)
 
 mask_layer = Lambda(lambda x: mask_layer(third_conv, b_mask), name='MaskLayer')(third_conv)
 
-reshape_layer = Lambda(lambda x: tf.reshape(mask_layer, [-1, 4], name='Reshape'))(mask_layer)
+reshape_layer = Lambda(lambda x: tf.reshape(mask_layer, [-1, 4]), name='Reshape')(mask_layer)
 
 tsi = Input(shape=[None], dtype='int64', name='TSIDecisionImages')
 
 nongreen_layer = Lambda(lambda x: tf.not_equal(tsi, 4), name='NonGreenLayer')(tsi)
 
-boolean_layer_one = Lambda(lambda x: tf.boolean_mask(reshape_layer, nongreen_layer), name='FirstBoolean')([reshape_layer, nongreen_layer])
+network_boolean = Lambda(lambda x: tf.boolean_mask(reshape_layer, nongreen_layer), name='NetworkBoolean')([reshape_layer, nongreen_layer])
 
-boolean_layer_two = Lambda(lambda x: tf.boolean_mask(tsi, nongreen_layer), name='SecondBoolean')([tsi, nongreen_layer])
+tsi_boolean = Lambda(lambda x: tf.boolean_mask(tsi, nongreen_layer), name='TSIBoolean')([tsi, nongreen_layer])
 
-s_s_cross_entropy_w_l = Lambda(lambda x: tf.nn.softmax_cross_entropy_with_logits(labels=boolean_layer_two, logits=boolean_layer_one), name='SparseSoftmaxCrossEntropy')([boolean_layer_two, boolean_layer_one])
+s_s_cross_entropy_w_l = Lambda(lambda x: tf.nn.softmax_cross_entropy_with_logits(labels=tsi_boolean, logits=network_boolean), name='SparseSoftmaxCrossEntropy')([tsi_boolean, network_boolean])
 
-cross_entropy = Lambda(lambda x: tf.reduce_mean(s_s_cross_entropy_w_l), name='FirstReduceMean')(s_s_cross_entropy_w_l)
+cross_entropy = Lambda(lambda x: tf.reduce_mean(s_s_cross_entropy_w_l), name='CrossEntropy')(s_s_cross_entropy_w_l)
 
-arg_max = Lambda(lambda x: tf.argmax(boolean_layer_one, 1), name='ArgMax')(boolean_layer_one)
+arg_max = Lambda(lambda x: tf.argmax(network_boolean, 1), name='ArgMax')(network_boolean)
 
-correct_prediction = Lambda(lambda x: tf.equal(arg_max, boolean_layer_two), name='CorrectPrediction')([arg_max, boolean_layer_two])
+correct_prediction = Lambda(lambda x: tf.equal(arg_max, tsi_boolean), name='CorrectPrediction')([arg_max, tsi_boolean])
 
 cast = Lambda(lambda x: tf.cast(correct_prediction, tf.float32), name='Cast')(correct_prediction)
 
-accuracy = Lambda(lambda x: tf.reduce_mean(cast), name='SecondReduceMean')(cast)
+accuracy = Lambda(lambda x: tf.reduce_mean(cast), name='Accuracy')(cast)
 
-model = Model(inputs=[first_input, tsi], outputs=[cross_entropy,  accuracy])
+model = Model(inputs=[sky_images, tsi], outputs=[cross_entropy, accuracy])
 
 model.summary()
-plot_model(model, to_file='model_1.png')
+plot_model(model, show_shapes=True, to_file='model_1.png')
