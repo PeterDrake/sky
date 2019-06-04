5a72997446e95ef26446f509732fe4356eb14534
Leon Rogge <leonrogge@lclark.edu>
2019-06-04 14:55:15 -0700

    Trying to set up multi_gpu usage. Keras and tensorflow versions where causing problems so changed code to only use native keras ATTEMPT #7

--- multi_gpu_mnist.py
+++ multi_gpu_mnist.py
@@ -1,25 +1,63 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow as tf
-from keras.utils import multi_gpu_model
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 
-if __name__ == '__main__':
-	mnist = tf.keras.datasets.mnist
+batch_size = 128
+num_classes = 10
+epochs = 12
 
-	(x_train, y_train), (x_test, y_test) = mnist.load_data()
-	x_train, x_test = x_train / 255.0, x_test / 255.0
+# input image dimensions
+img_rows, img_cols = 28, 28
 
-	model = tf.keras.models.Sequential([
-		tf.keras.layers.Flatten(input_shape=(28, 28)),
-		tf.keras.layers.Dense(512, activation=tf.nn.relu),
-		tf.keras.layers.Dropout(0.2),
-		tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-	])
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-	parallel_model = multi_gpu_model(model, gpus=4)
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
 
-	parallel_model.compile(optimizer='adam',
-				  loss='sparse_categorical_crossentropy',
-				  metrics=['accuracy'])
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 
-	parallel_model.fit(x_train, y_train, epochs=5)
-	parallel_model.evaluate(x_test, y_test)
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
--- multi_gpu_mnist_launch.py
+++
@@ -1,7 +0,0 @@
-import os
-from config import *
-
-
-if __name__ == "__main__":
-	for i in range(NUM_NETWORKS):
-		os.system('python3 multi_gpu_mnist.py')
