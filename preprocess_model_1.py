af4f77d792a4d280905da82e97e7e4858595e816
acschall <schall@lclark.edu>
2019-06-04 17:07:29 -0700

    Added debugging print statements to preprocess.

--- preprocess_model_1.py
+++ preprocess_model_1.py
@@ -19,11 +19,15 @@ from train import *
 from model_1 import *
 
 
-def load_tsi(stamps, input_dir):
+def load_tsi(stamps, input_dir, name):
 	masks = np.empty((len(stamps), 480, 480))
 	for i, s in enumerate(stamps):
 		masks[i] = mask_to_index(np.asarray(imageio.imread(extract_mask_path_from_time(s, input_dir))))
 		print('TSI Progress: ' + str(i/len(stamps)))
+	print('SHAPE: ')
+	print(masks.shape)
+	with open('Keras-Data' + '/' + name + '_tsi_masks', 'wb') as f:
+		pickle.dump(masks, f)
 	non_green = tf.not_equal(masks.reshape((-1)), 4)
 	return tf.boolean_mask(masks, non_green)
 
@@ -48,11 +52,11 @@ if __name__ == '__main__':
 
 	training_filenames = load_filenames(train_stamps, TYPICAL_DATA_DIR)
 	print('Training files loaded.')
-	training_tsi_labels = load_tsi(train_stamps, TYPICAL_DATA_DIR)
+	training_tsi_labels = load_tsi(train_stamps, TYPICAL_DATA_DIR, 'training')
 	print('Training labels loaded.')
 	validation_filenames = load_filenames(valid_stamps, TYPICAL_DATA_DIR)
 	print('Validation files loaded.')
-	validation_tsi_labels = load_tsi(valid_stamps, TYPICAL_DATA_DIR)
+	validation_tsi_labels = load_tsi(valid_stamps, TYPICAL_DATA_DIR, 'validation')
 	print('Validation labels loaded.')
 
 	with open('Keras-Data' + '/training_filenames', 'wb') as f:
