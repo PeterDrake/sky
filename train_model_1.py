49dfe83927b342feeb5995d5cee55cf0aaf13760
Leon Rogge <leonrogge@lclark.edu>
2019-06-07 13:58:53 -0700

    Updated print statement for debugging sky_images.shape

--- train_model_1.py
+++ train_model_1.py
@@ -47,7 +47,7 @@ class Image_Generator(Sequence):
 		print(sky_images.shape)
 		print('MASKS SHAPE: ')
 		print(masks.shape)
-		print('Test: ' + len(self.image_filenames)/len(self.batch_size))
+		print('Test: ' + len(self.image_filenames)/float(self.batch_size))
 		X = [sky_images, masks]
 
 		graph = tf.Graph()
