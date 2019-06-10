fed77ceafdff647604523dc3f7ba08bbb8023c62
Peter Drake <drake@lclark.edu>
2019-06-10 14:53:34 -0700

    Cleaning up model_1.py

--- model_1.py
+++ model_1.py
@@ -6,52 +6,39 @@
 Builds the model.
 """
 
-from keras.models import Model
-from keras.layers import Convolution2D, concatenate, Input, Lambda
-from keras.utils import plot_model, multi_gpu_model
-import tensorflow as tf
-from config import *
+from tensorflow._api.v1.keras.models import Model
+from tensorflow._api.v1.keras.layers import Convolution2D, concatenate, Input, Lambda
+from tensorflow._api.v1.keras.utils import plot_model, multi_gpu_model
 from train import *
 
 
 def build_model():
-	"""Builds the network created by Jeff and Sean."""
-
-	''' Create the black mask to be added to the output of the third convolutional layer. '''
-	b_mask = color_mask(np.asarray(imageio.imread(TYPICAL_DATA_DIR + '/always_black_mask.png')), index_of(BLACK, COLORS))
-	black = tf.constant(b_mask)
-
-	''' Create the inputs to the network. '''
+	"""Builds and returns the network."""
+	# Create the inputs to the network.
 	sky_images = Input(shape=(480, 480, 3), name='SkyImages')
-	tsi = Input(shape=(480, 480), dtype='int64', name='TSIDecisionImages')
-
-	''' Create the main body of the network. '''
-	first_conv = Convolution2D(filters=32, kernel_size=3, padding='same', data_format='channels_last', activation='relu', name='FirstConvolution')(sky_images)
-	first_maxpool = Lambda(lambda x: tf.nn.max_pool(first_conv, [1, 1, 100, 1], strides=[1, 1, 1, 1], padding='SAME'), name='FirstMaxPool')(first_conv)
-	second_maxpool = Lambda(lambda x: tf.nn.max_pool(first_conv, [1, 100, 1, 1], strides=[1, 1, 1, 1], padding='SAME'), name='SecondMaxPool')(first_conv)
-	merge_one = concatenate([first_conv, first_maxpool], axis=3)
-	merge_two = concatenate([second_maxpool, merge_one], axis=3)
-	second_conv = Convolution2D(filters=32, kernel_size=3, padding='same', data_format='channels_last', activation='relu', name='SecondConvolution')(merge_two)
-	merge_three = concatenate([second_conv, sky_images], axis=3)
-	third_conv = Convolution2D(filters=4, kernel_size=3, padding='same', data_format='channels_last', activation='relu', name='ThirdConvolution')(merge_three)
-
-	''' Add the always_black_mask to the output of the third convolutional layer. Calls mask_layer from train.py'''
-	mask = Lambda(lambda x: tf.add(black, third_conv), name='MaskLayer')(third_conv)
-
-	''' Makes a boolean array with all the pixels that are not green set to true. 
-	Necessary because of the green lines on the TSI decision image.'''
-	nongreen_layer = Lambda(lambda x: tf.not_equal(tsi, np.full((TRAINING_BATCH_SIZE, 480, 480), 4)), name='NonGreenLayer')(tsi)
-
-	''' Takes a condition and two arrays of the same shape. The condition is an boolean array with the same shape as the
-	other two arrays. For each element in condition,  if the value is True, the corresponding value from the first array
-	is kept. If the value is False, the corresponding value from the second array is kept. 
-	Returns a tensor of the same size as the arrays populated with the chosen elements.'''
-	network_boolean = Lambda(lambda x: tf.where(tf.stack([nongreen_layer, nongreen_layer, nongreen_layer, nongreen_layer], axis=3), mask, tf.zeros_like(mask, dtype='float32')), name='NetworkBoolean')([nongreen_layer, mask])
-
-	model = Model(inputs=[sky_images, tsi], outputs=network_boolean)
-
+	tsi = Input(shape=(480, 480), dtype='uint8', name='TSIDecisionImages') # TSI's decision images
+	# Main body of the network
+	conv1 = Convolution2D(filters=32, kernel_size=3, padding='same', data_format='channels_last', activation='relu')(sky_images)
+	maxpool1 = Lambda(lambda x: tf.nn.max_pool(conv1, [1, 1, 100, 1], strides=[1, 1, 1, 1], padding='SAME'), name='maxpool1')(conv1)
+	maxpool2 = Lambda(lambda x: tf.nn.max_pool(conv1, [1, 100, 1, 1], strides=[1, 1, 1, 1], padding='SAME'), name='maxpool2')(conv1)
+	concat1 = concatenate([conv1, maxpool1], axis=3)
+	concat2 = concatenate([maxpool2, concat1], axis=3)
+	conv2 = Convolution2D(filters=32, kernel_size=3, padding='same', data_format='channels_last', activation='relu')(concat2)
+	concat3 = concatenate([conv2, sky_images], axis=3)
+	conv3 = Convolution2D(filters=4, kernel_size=3, padding='same', data_format='channels_last', activation='relu')(concat3)
+	# Mask to mark pixels that are ALWAYS black to be black
+	b_mask = color_mask(np.asarray(imageio.imread(TYPICAL_DATA_DIR + '/always_black_mask.png')),
+						index_of(BLACK, COLORS))
+	always_black = tf.constant(b_mask)
+	masked = Lambda(lambda x: tf.add(always_black, conv3), name='masked')(conv3)
+	# Determine which pixels in TSI decision image are non-green
+	nongreen = Lambda(lambda x: tf.not_equal(tsi, np.full((TRAINING_BATCH_SIZE, 480, 480), 4)), name='nongreen')(tsi)
+	# Output of the network, where each pixel has a probability for each color, but all probabilities are zero
+	# for pixels that are green in TSI decision image
+	logits_without_green = Lambda(lambda x: tf.where(tf.stack([nongreen, nongreen, nongreen, nongreen], axis=3), masked, tf.zeros_like(masked, dtype='float32')), name='logits_without_green')([nongreen, masked])
+	# Build and return the model
+	model = Model(inputs=[sky_images, tsi], outputs=logits_without_green)
 	# model = multi_gpu_model(model, gpus=4)
-
 	return model
 
 
