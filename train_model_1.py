d7e947bd80daed617e1fcb11bdbc7f0b0bdcad53
acschall <schall@lclark.edu>
2019-06-10 16:12:04 -0700

    Added early stopping callback.

--- train_model_1.py
+++ train_model_1.py
@@ -99,19 +99,18 @@ if __name__ == '__main__':
 	validation_batch_generator = Image_Generator(validation_image_filenames, validation_tsi_filenames, TRAINING_BATCH_SIZE)
 	print('Validation generator initialized.')
 
-	# cb_1 = EarlyStopping(monitor='val_loss')
-	#
-	# cb_2 = ModelCheckpoint(filepath='/'+run_name+'/checkpoint-{epoch:02d}-{val_loss:.2f}.hdf5')
+	cb_1 = EarlyStopping(monitor='val_loss')
 
 	model.summary()
 
 	model.fit_generator(generator=training_batch_generator,
 						steps_per_epoch=(len(train_stamps) // (TRAINING_BATCH_SIZE)),
-						epochs=1,
+						epochs=2,
 						verbose=1,
 						validation_data=validation_batch_generator,
 						validation_steps=(len(valid_stamps) // (TRAINING_BATCH_SIZE)),
-						use_multiprocessing=False)
+						use_multiprocessing=False,
+						callbacks=[cb_1])
 
 	model.save('model_1_2.h5')
 
