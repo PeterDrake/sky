9cf33d8308967c89a7f5edad4d4fad0a07387b37
acschall <schall@lclark.edu>
2019-06-12 14:30:00 -0700

    Changed custom objects.

--- make_decision_images.py
+++ make_decision_images.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
 	# 	model = load_model('model_1_4.h5')
 
 	# custom = {'not_green': 'NotGreen', 'remove_green': 'RemoveGreen', 'decide_pixel_colors': 'DecidePixelColors'}
-	custom = {'not_green': NotGreen, 'remove_green': RemoveGreen, 'decide_pixel_colors': DecidePixelColors}
+	custom = {'NotGreen': NotGreen, 'RemoveGreen': RemoveGreen, 'DecidePixelColors': DecidePixelColors}
 
 	model = tf._api.v1.keras.models.load_model('model_1_5.h5', custom_objects=custom)
 
