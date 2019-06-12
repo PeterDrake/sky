e123b4d0f3e1196593d1105842d572ff1560152c
acschall <schall@lclark.edu>
2019-06-11 21:18:12 -0700

    Tried changing lambda layer format to see if it fixes model saving error. Changed run name to keras_train_8.

--- train_model_1_launch.py
+++ train_model_1_launch.py
@@ -6,5 +6,5 @@ import os
 from config import *
 
 if __name__ == "__main__":
-	run_name = 'keras_train_7'
+	run_name = 'keras_train_8'
 	os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 -u train_model_1.py {}" -P {}'.format(run_name, run_name, JOB_PRIORITY))
