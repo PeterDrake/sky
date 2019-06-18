0aed94c13fed6f098a0534b56e49b7486610d783
Leon Rogge <leonrogge@lclark.edu>
2019-06-18 14:02:11 -0700

    added radius and circle_center to data frame

--- hough_circle_all_img.py
+++ hough_circle_all_img.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from config import RAW_DATA_DIR, TYPICAL_DATA_DIR
 from utils import extract_img_path_from_time_raw
+import pandas as pd
 
 
 def hough_preprocess(filename):
@@ -17,13 +18,14 @@ def hough_preprocess(filename):
 		radius, circle_center = hough_circle(time, RAW_DATA_DIR)
 		print('radius: ' + str(radius))
 		print('center of circle: ' + str(circle_center))
+		global data
+		data.append({'time': time, 'y': circle_center[1], 'x': circle_center[0], 'radius': radius})
 	file.close()
 	print("Finished preprocessing sky and decision images in ", filename)
 
 
 def hough_circle(timestamp, input_dir):
 	img_path = extract_img_path_from_time_raw(timestamp, input_dir)
-	print('img_path: ' + img_path)
 	img = cv.imread(img_path, 0)
 
 	circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 100, param1=60, param2=90, minRadius=200, maxRadius=0)
@@ -31,21 +33,21 @@ def hough_circle(timestamp, input_dir):
 	circles = np.uint16(np.around(circles))
 
 	for i in circles[0]:
-
-		# center of circle
 		circle_center = (i[0], i[1])
-		print('center of circle: ' + str((i[0], i[1])))
-
-		# radius
 		radius = i[2]
-		print('radius: ' + str(i[2]) + '\n')
 
 	return radius, circle_center
+
 
 if __name__ == "__main__":
 	# batches = os.listdir(TYPICAL_DATA_DIR + '/res')
 	# for batch in enumerate(batches):
 	# 	hough_preprocess(TYPICAL_DATA_DIR + '/res/' + str(batch))
+
+	data = []
 	hough_preprocess(TYPICAL_DATA_DIR + '/res/batch0.txt')
+	hough_centers = pd.DataFrame(data)
+	hough_centers.to_csv('hough_centers.csv')
+	print(data)
 
 # SGE_Batch -r "hough_circle" -c "python3 -u hough_circle_all_img.py" -P 1
