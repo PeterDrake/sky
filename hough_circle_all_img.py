f302717a8e8ecd97764a542c56ea8b6e54657a73
Leon Rogge <leonrogge@lclark.edu>
2019-06-19 14:35:19 -0700

    decreased image blur to 5, minR = 220 maxR = 245, param1 = 90 param2 = 25

--- hough_circle_all_img.py
+++ hough_circle_all_img.py
@@ -29,7 +29,8 @@ def hough_circle(timestamp, input_dir):
 	img = cv.imread(img_path, 0)
 	img = cv.medianBlur(img, 5)
 
-	circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 120, param1=90, param2=25, minRadius=220, maxRadius=250)
+	circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 120, param1=90, param2=25,
+							  minRadius=220, maxRadius=245)
 
 	if circles is None:
 		return 0, (0, 0)
