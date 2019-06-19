90da7e63a75c9eac563f2e2e1b439ad7170d8f21
Leon Rogge <leonrogge@lclark.edu>
2019-06-19 13:36:57 -0700

    decreased image blur to 5, minR = 220 maxR = 245

--- hough_circle_one_img.py
+++ hough_circle_one_img.py
@@ -8,16 +8,19 @@ dirs = os.listdir(path)
 for file in dirs:
 	print(file)
 	img = cv2.imread(path + file, 0)
-	img = cv2.medianBlur(img, 11)
+	img = cv2.medianBlur(img, 7)
 	cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 	# minRadius=210, maxRadius=245
-	circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 120, param1=70, param2=50, minRadius=200, maxRadius=250)
+	circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 120, param1=70, param2=50, minRadius=220, maxRadius=245)
 
 	circles = np.uint16(np.around(circles))
 	print(len(circles))
 	for i in circles[0]:
 		# draw the outer circle
-		cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+		cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 1)
+		# test circle
+		cv2.circle(cimg, (i[0], i[1]), 250, (255, 255, 0), 1)
+		cv2.circle(cimg, (i[0], i[1]), 225, (255, 255, 0), 1)
 		# draw the center of the circle
 		cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
 		# center of circle
