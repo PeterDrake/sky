1cd3ca2cb2fb6e2d311fdf5c45d57cd134cd6555
acschall <schall@lclark.edu>
2019-06-20 12:48:47 -0700

    Changed short run to False and set epochs to 50.

--- train_model_1.py
+++ train_model_1.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
 	cb_1 = EarlyStopping(monitor='val_loss')
 
 	history = model.fit_generator(generator=training_batch_generator,
-						steps_per_epoch=len(train_stamps) // TRAINING_BATCH_SIZE, epochs=2, verbose=1,
+						steps_per_epoch=len(train_stamps) // TRAINING_BATCH_SIZE, epochs=50, verbose=1,
 						validation_data=validation_batch_generator,
 						validation_steps=len(valid_stamps) // TRAINING_BATCH_SIZE,
 						use_multiprocessing=False, callbacks=[cb_1])
