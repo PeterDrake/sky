5a2ed66135b30906488cf1a57e47c8a756895f12
acschall <schall@lclark.edu>
2019-06-21 09:06:45 -0700

    Tried adding tensorboard again. On model 11 and keras train 17.

--- train_model_1.py
+++ train_model_1.py
@@ -10,7 +10,7 @@ Trains the model.
 
 from tensorflow._api.v1.keras.utils import to_categorical
 from tensorflow.python.keras.utils.data_utils import Sequence
-from tensorflow._api.v1.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow._api.v1.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 # from keras.utils import to_categorical
 # from keras.utils.data_utils import Sequence
 # from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
@@ -134,26 +134,31 @@ if __name__ == '__main__':
 
 	cb_1 = EarlyStopping(monitor='val_loss')
 
+	tensorboard = TensorBoard(log_dir='./logs',
+							  histogram_freq=1,
+							  write_graph=True,
+							  write_images=False)
+
 	history = model.fit_generator(generator=training_batch_generator,
 						steps_per_epoch=len(train_stamps) // TRAINING_BATCH_SIZE, epochs=50, verbose=1,
 						validation_data=validation_batch_generator,
 						validation_steps=len(valid_stamps) // TRAINING_BATCH_SIZE,
-						use_multiprocessing=False, callbacks=[cb_1])
+						use_multiprocessing=False, callbacks=[cb_1, tensorboard])
 
 
-	print(history.history)
+	# print(history.history)
+	#
+	# train_history = {}
+	#
+	# for key, value in history.history.items():
+	# 	train_history[key] = []
+	# 	for number in value:
+	# 		train_history[key].append(float(number))
+	#
+	#
+	#
+	# with open('history16/history.json', 'w') as fp:
+	# 	json.dump(train_history, fp)
 
-	train_history = {}
-
-	for key, value in history.history.items():
-		train_history[key] = []
-		for number in value:
-			train_history[key].append(float(number))
-
-
-
-	with open('history16/history.json', 'w') as fp:
-		json.dump(train_history, fp)
-
-	model.save('model_1_10.h5')
+	model.save('model_1_11.h5')
 
