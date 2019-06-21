13a30dfa7aeac77c3b07422ceb0c17991a72a458
acschall <schall@lclark.edu>
2019-06-21 10:35:10 -0700

    Changed import order.

--- tensorboard.py
+++ tensorboard.py
@@ -1,10 +1,10 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
 import tensorflow as tf
 from tensorflow._api.v1.keras.callbacks import *
 import tensorflow._api.v1.keras as K
 
-
-from __future__ import absolute_import
-from __future__ import print_function
 
 import os
 import csv
