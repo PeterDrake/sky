58e99b34e9f27d5818f60623e3f0263952d3b6a4
acschall <schall@lclark.edu>
2019-06-24 14:09:06 -0700

    Added file to launch preprocessing.

---
+++ center_preprocess_launch.py
@@ -0,0 +1,24 @@
+import os
+import time
+from config import *
+from center_preprocess import preprocess
+
+
+if __name__ == "__main__":
+	start = time.clock()
+	# Runs preprocessing for typical data
+	batches = os.listdir(TYPICAL_DATA_DIR + '/res')
+	for i, batch in enumerate(batches):
+		if BLT:
+			os.system('SGE_Batch -r "{}" -c "python3 -u center_preprocess.py {} {}" -P 1'.format('typical-pre-batch-{}'.format(i), TYPICAL_DATA_DIR + '/res/' + batch, TYPICAL_DATA_DIR))
+		else:
+			preprocess(TYPICAL_DATA_DIR + '/res/' + batch, TYPICAL_DATA_DIR)
+
+	# Runs preprocessing for dubious data
+	batches = os.listdir(DUBIOUS_DATA_DIR + '/res')
+	for i, batch in enumerate(batches):
+		if BLT:
+			os.system('SGE_Batch -r "{}" -c "python3 -u center_preprocess.py {} {}" -P 1'.format('dubious-pre-batch-{}'.format(i), DUBIOUS_DATA_DIR + '/res/' + batch, DUBIOUS_DATA_DIR))
+		else:
+			preprocess(DUBIOUS_DATA_DIR + '/res/' + batch, DUBIOUS_DATA_DIR)
+	print("Time elapsed: " + str(time.clock() - start) + " seconds.")
