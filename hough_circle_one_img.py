324a2c45d8a8fe90efa8cf3214056b3637af64d3
Leon Rogge <leonrogge@lclark.edu>
2019-06-26 09:30:17 -0700

    fixing bug that points to what images to use

--- hough_circle_one_img.py
+++ hough_circle_one_img.py
@@ -7,7 +7,7 @@ path = "typical_data/hough_circle_sample_files/"
 dirs = os.listdir(path)
 
 for file in dirs:
-	print(file)
+	# print(file)
 	img = cv2.imread(path + file, 0)
 	img = cv2.medianBlur(img, 11)
 	cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
@@ -16,7 +16,6 @@ for file in dirs:
 							   minRadius=224, maxRadius=243)
 
 	circles = np.uint16(np.around(circles))
-	print(len(circles))
 	closest_to_center = (240, 320)
 	old_distance = 60
 	for i in circles[0, :]:
@@ -29,20 +28,21 @@ for file in dirs:
 			radius = i[2]
 
 		# draw the outer circle
-		cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 1)
-		# test circle
-		cv2.circle(cimg, (i[0], i[1]), 245, (255, 255, 0), 1)
-		cv2.circle(cimg, (i[0], i[1]), 220, (255, 255, 0), 1)
-		# draw the center of the circle
-		cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
-		# center of circle
-		print('center of circle: ' + str((i[0], i[1])))
-		# radius
-		print('radius: ' + str(i[2]))
+		# cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 1)
+		# # test circle
+		# cv2.circle(cimg, (i[0], i[1]), 245, (255, 255, 0), 1)
+		# cv2.circle(cimg, (i[0], i[1]), 220, (255, 255, 0), 1)
+		# # draw the center of the circle
+		# cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+		# # center of circle
 
 	cv2.circle(cimg, (closest_to_center[0], closest_to_center[1]), 2, (255, 0, 255), 3)
 	cv2.circle(cimg, (closest_to_center[0], closest_to_center[1]), radius, (255, 0, 255), 1)
 	cv2.circle(cimg, (240, 320), 60, (255, 0, 255), 1)
+
+	print('{} {} {} {}'.format(file, closest_to_center[0], closest_to_center[1], radius))
+	# radius
+	# print('radius: ' + str(i[2]))
 
 
 	cv2.imshow('detected circles', cimg)
