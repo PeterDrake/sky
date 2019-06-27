f4f5504217afe279b480b9193647e6baedd1f1f7
acschall <schall@lclark.edu>
2019-06-27 10:35:58 -0700

    Changed job priority to 1 for training model 2.

--- train_model_2_launch.py
+++ train_model_2_launch.py
@@ -7,4 +7,4 @@ from config import *
 
 if __name__ == "__main__":
 	run_name = 'inception_1'
-	os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 -u train_model_2.py {}" -P {}'.format(run_name, False, JOB_PRIORITY))
+	os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 -u train_model_2.py {}" -P {}'.format(run_name, False, 1))
