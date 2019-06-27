ac9846bbcf097bba58e7b4a3e91766b2efd0475c
acschall <schall@lclark.edu>
2019-06-27 10:25:10 -0700

    Added files to train the inception model.

---
+++ train_model_2.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# written by Anna Schall
+# started 6/4/19
+# used code from train.py at the Sky repository
+# used code from https://medium.com/datadriveninvestor/keras-training-on-large-datasets-3e9d9dbc09d4
+"""
+Trains the model.
+"""
+
+from tensorflow._api.v1.keras.utils import to_categorical
+from tensorflow.python.keras.utils.data_utils import Sequence
+from tensorflow._api.v1.keras.callbacks import EarlyStopping
+from tensorboard import TensorBoard
+import tensorflow as tf
+from model_2 import build_model
+from utils import *
+from config import *
+from train import mask_to_index
+import pickle
+import sys
+
+
+
+class Image_Generator(Sequence):
+	""" Creates a generator that fetches the batches of data. """
+
+	def __init__(self, image_filenames, label_filenames, batch_size):
+		self.image_filenames, self.label_filenames = image_filenames, label_filenames
+		self.batch_size = batch_size
+
+	def __len__(self):
+		# return int(np.ceil(len(self.image_filenames) / float(self.batch_size)))
+		return int(np.floor(len(self.image_filenames) / float(self.batch_size)))
+
+
+	def __getitem__(self, idx):
+		''' Grabs the correct image and label image files for the batch. '''
+		x_filenames = self.image_filenames[idx * self.batch_size:(idx + 1) * self.batch_size]
+		y_filenames = self.label_filenames[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+		''' Makes an array of arrays where each element is an image in numpy array format. '''
+		sky_images = np.array([np.asarray(imageio.imread(file_name)) for file_name in x_filenames])
+		''' Makes an array of arrays where each element is an label image in numpy array format.'''
+		tsi = np.array([np.asarray(imageio.imread(file_name)) for file_name in y_filenames])
+
+		''' Converts each pixel label to a number based on color; key is found in utils. WHITE is 0, BLUE is 1, 
+		GRAY is 2, BLACK is 3, and GREEN is 4.'''
+		masks = np.empty((self.batch_size, 480, 480))
+		for i in range(len(tsi)):
+			masks[i] = mask_to_index(tsi[i])
+
+		X = [sky_images, masks]
+
+		''' Converts each pixel label to an array where the index indicates what color and a 1 indicates that the 
+		pixel is that color. The array for each pixel should only have one 1 and the other elements should be zeros. 
+		The array is now of the size (batch_size)x480x480x5. '''
+		Y = to_categorical(masks)
+
+		''' Slices off the final index of the final dimension which indicates the color green. 
+		The array is now of the size (batch_size)x480x480x4. '''
+		Y = Y[:, :, :, 0:4]
+
+		return X, Y
+
+
+def corrected_accuracy(y_true, y_pred):
+	green = tf.constant([[0 for i in range(480)] for j in range(480)], dtype='float32')
+	mask = tf.not_equal(tf.reduce_sum(y_true, axis=-1), green)
+	correct = tf.cast(tf.equal(tf.argmax(tf.boolean_mask(y_true, mask), axis=-1), tf.argmax(tf.boolean_mask(y_pred, mask), axis=-1)), tf.float32)
+	return tf.count_nonzero(correct)/tf.size(correct, out_type=tf.dtypes.int64)
+
+
+def load_filenames(stamps, input_dir, masks):
+	filenames = []
+	if masks:
+		for s in stamps:
+			filenames.append(extract_mask_path_from_time(s, input_dir))
+	else:
+		for s in stamps:
+			filenames.append(extract_img_path_from_time(s, input_dir))
+	return filenames
+
+
+if __name__ == '__main__':
+	short_run = sys.argv[1]
+	print(short_run)
+
+	with open(TYPICAL_DATA_DIR + '/train.stamps', 'rb') as f:
+		train_stamps = pickle.load(f)
+	print('Training stamps loaded.')
+	with open(TYPICAL_VALID_FILE, 'rb') as f:
+		valid_stamps = pickle.load(f)
+	print('Validation stamps loaded.')
+
+	if short_run == 'True':
+		print('SHORT RUN SET TO TRUE.')
+		train_stamps = train_stamps[0:1000]
+		valid_stamps = valid_stamps[0:300]
+
+	training_image_filenames = load_filenames(train_stamps, TYPICAL_DATA_DIR, False)
+	print('Training image file paths loaded.')
+	print(len(training_image_filenames))
+	training_tsi_filenames = load_filenames(train_stamps, TYPICAL_DATA_DIR, True)
+	print('Training mask file paths loaded.')
+	print(len(training_tsi_filenames))
+	validation_image_filenames = load_filenames(valid_stamps, TYPICAL_DATA_DIR, False)
+	print('Validation image file paths loaded.')
+	validation_tsi_filenames = load_filenames(valid_stamps, TYPICAL_DATA_DIR, True)
+	print('Validation mask file paths loaded.')
+
+	losses = {
+		"remove_green": "categorical_crossentropy",
+	}
+
+	metrics = {
+		"remove_green": corrected_accuracy,
+	}
+
+	model = build_model()
+	print('Model built.')
+	model.compile(optimizer='adam', loss=losses, metrics=metrics)
+	print('Model compiled.')
+
+	training_batch_generator = Image_Generator(training_image_filenames, training_tsi_filenames, TRAINING_BATCH_SIZE)
+	print('Training generator initialized.')
+	validation_batch_generator = Image_Generator(validation_image_filenames, validation_tsi_filenames, TRAINING_BATCH_SIZE)
+	print('Validation generator initialized.')
+
+	cb_1 = EarlyStopping(monitor='val_loss')
+
+	tensorboard = TensorBoard(log_dir='./logs',
+							  histogram_freq=0,
+							  write_graph=True,
+							  write_images=False,
+							  write_batch_performance=True)
+
+	history = model.fit_generator(generator=training_batch_generator,
+						steps_per_epoch=len(train_stamps) // TRAINING_BATCH_SIZE, epochs=2, verbose=1,
+						validation_data=validation_batch_generator,
+						validation_steps=len(valid_stamps) // TRAINING_BATCH_SIZE,
+						use_multiprocessing=False, callbacks=[cb_1, tensorboard])
+
+	model.save('model_2_1.h5')
---
+++ train_model_2_launch.py
@@ -0,0 +1,10 @@
+"""
+Launches the training process.
+"""
+
+import os
+from config import *
+
+if __name__ == "__main__":
+	run_name = 'inception_1'
+	os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 -u train_model_2.py {}" -P {}'.format(run_name, False, JOB_PRIORITY))
