a1811b03914da0d20ebeff19342e793ae5bad072
Leon Rogge <leonrogge@lclark.edu>
2019-07-02 17:00:19 -0700

    made unit test for removing green lines and updated the replace_green_lines to use squares to scan the whole image and votes on the color which occurs the most

--- replace_green_lines.py
+++ replace_green_lines.py
@@ -1,37 +1,71 @@
-from utils import *
-from config import *
-import pandas as pd
-import numpy as np
 from preprocess import *
-import PIL
-from PIL import ImageDraw
-import random
+from collections import Counter
 import numpy_indexed as npi
 
 
-mask = np.asarray(imageio.imread('typical_data/green_mask_test.png', pilmode="RGB"))
+def remove_green():
+
+	mask = np.asarray(imageio.imread('', pilmode="RGB"))
+
+	for i in range(480):
+		for j in range(480):
+			choices = []
+			if np.array_equal(mask[i][j], GREEN):
+				for r in range(1, 5):
+					if not np.array_equal(mask[i][j - r], GREEN) and not np.array_equal(mask[i][j - r], BLACK):
+						choices.append(mask[i][j - r])
+					if not np.array_equal(mask[i][j + r], GREEN) and not np.array_equal(mask[i][j + r], BLACK):
+						choices.append(mask[i][j + r])
+					if not np.array_equal(mask[i - r][j], GREEN) and not np.array_equal(mask[i - r][j], BLACK):
+						choices.append(mask[i - r][j])
+					if not np.array_equal(mask[i + r][j], GREEN) and not np.array_equal(mask[i + r][j], BLACK):
+						choices.append(mask[i + r][j])
+					if len(choices) != 0:
+						break
+				np.array(choices)
+				row = npi.mode(choices)
+				mask[i][j] = row
+
+	imageio.imwrite('typical_data/green_mask_results.png', mask)
 
 
-for i in range(480):
-	for j in range(480):
-		choices = []
-		r = 1
-		if np.array_equal(mask[i][j], GREEN):
-			for r in range(1, 5):
-				if not np.array_equal(mask[i][j - r], GREEN) and not np.array_equal(mask[i][j - r], BLACK):
-					choices.append(mask[i][j - r])
-				if not np.array_equal(mask[i][j + r], GREEN) and not np.array_equal(mask[i][j + r], BLACK):
-					choices.append(mask[i][j + r])
-				if not np.array_equal(mask[i - r][j], GREEN) and not np.array_equal(mask[i - r][j], BLACK):
-					choices.append(mask[i - r][j])
-				if not np.array_equal(mask[i + r][j], GREEN) and not np.array_equal(mask[i + r][j], BLACK):
-					choices.append(mask[i + r][j])
-				if len(choices) != 0:
-					break
-			np.array(choices)
-			row = npi.mode(choices)
-			mask[i][j] = row
+def new_remove_green(mask):
+
+	new_mask = np.zeros((mask.shape[0], mask.shape[0], 3), dtype=np.uint8)
+
+	for i in range(mask.shape[0]):
+		for j in range(mask.shape[0]):
+			if np.array_equal(mask[i][j], GREEN):
+				# This is the size of the starting square size that gets voted on
+				radius = 1
+
+				while True:
+
+					window = (mask[i - radius:i + radius + 1, j - radius:j + radius + 1])
+					choices = Counter()
+					for x in range(radius * 2 + 1):
+						for y in range(radius * 2 + 1):
+							if np.array_equal(window[x][y], BLUE):
+								choices['BLUE'] += 1
+							elif np.array_equal(window[x][y], GRAY):
+								choices['GRAY'] += 1
+							elif np.array_equal(window[x][y], WHITE):
+								choices['WHITE'] += 1
+					if choices:
+						break
+					else:
+						radius += 1
+
+				color, _ = choices.most_common(1)[0]
+				new_mask[i][j] = eval(color)
+
+			else:
+				new_mask[i][j] = mask[i][j]
+
+	return new_mask
 
 
-
-imageio.imwrite('i_like_waffels.png', mask)
+if __name__ == '__main__':
+	# remove_green()
+	m = np.asarray(imageio.imread('typical_data/green_mask_test.png', pilmode="RGB"))
+	imageio.imwrite('typical_data/green_mask_results_new.png', new_remove_green(m))
