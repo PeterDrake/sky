e585e235dce9c37611748e22800367506c381657
Leon Rogge <leonrogge@lclark.edu>
2019-07-02 17:19:32 -0700

    made another version of the remove_green to show what happens if no new_mask is created

--- replace_green_lines.py
+++ replace_green_lines.py
@@ -5,6 +5,30 @@ import numpy_indexed as npi
 
 def remove_green(mask):
 
+	for i in range(480):
+		for j in range(480):
+			choices = []
+			if np.array_equal(mask[i][j], GREEN):
+				for r in range(1, 5):
+					if not np.array_equal(mask[i][j - r], GREEN) and not np.array_equal(mask[i][j - r], BLACK):
+						choices.append(mask[i][j - r])
+					if not np.array_equal(mask[i][j + r], GREEN) and not np.array_equal(mask[i][j + r], BLACK):
+						choices.append(mask[i][j + r])
+					if not np.array_equal(mask[i - r][j], GREEN) and not np.array_equal(mask[i - r][j], BLACK):
+						choices.append(mask[i - r][j])
+					if not np.array_equal(mask[i + r][j], GREEN) and not np.array_equal(mask[i + r][j], BLACK):
+						choices.append(mask[i + r][j])
+					if len(choices) != 0:
+						break
+				np.array(choices)
+				row = npi.mode(choices)
+				mask[i][j] = row
+
+	return mask
+
+
+def remove_green_v2(mask):
+
 	new_mask = np.copy(mask)
 
 	for i in range(480):
@@ -64,5 +88,6 @@ def new_remove_green(mask):
 
 if __name__ == '__main__':
 	m = np.asarray(imageio.imread('typical_data/green_mask_test.png', pilmode="RGB"))
-	imageio.imwrite('typical_data/green_mask_results.png', remove_green(m))
-	# imageio.imwrite('typical_data/green_mask_results_new.png', new_remove_green(m))
+	imageio.imwrite('typical_data/green_mask_results_v3.png', new_remove_green(m))
+	imageio.imwrite('typical_data/green_mask_results_v2.png', remove_green_v2(m))
+	imageio.imwrite('typical_data/green_mask_results_v1.png', remove_green(m))
