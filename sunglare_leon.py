13fdbf28406d1643e12f0bbf091fba268faf0a31
E00S <leonrogge@lclark.edu>
2019-07-05 08:50:19 -0700

    preprocess imgs for dealing with sun glare

--- sunglare_leon.py
+++ sunglare_leon.py
@@ -1,3 +1,14 @@
 from replace_green_lines import new_remove_green
 from center_preprocess import *
 
+
+def test():
+    m = np.asarray(imageio.imread('typical_data/sgptsicldmaskC1.a1.20120501.170000.png.20120501170000.png', pilmode="RGB"))
+    img = np.asarray(imageio.imread('typical_data/sgptsiskyimageC1.a1.20120501.170000.jpg.20120501170000.jpg', pilmode="RGB"))
+    m, img = center_and_add_border(m, img)
+    imageio.imwrite('typical_data/mask_20120501170000_sunglare_v1.png', new_remove_green(m))
+    imageio.imwrite('typical_data/img_20120501170000_sunglare_v1.jpg', img)
+
+
+if __name__ == '__main__':
+    test()
