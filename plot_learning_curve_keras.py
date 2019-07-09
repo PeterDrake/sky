7ca8f0ffb3c22bda8083f03a868646bbadeac58e
Leon Rogge <leonrogge@lclark.edu>
2019-07-09 14:03:11 -0700

    created file to plot accuracy of training. This is a test to see if loading the model from training works

---
+++ plot_learning_curve_keras.py
@@ -0,0 +1,14 @@
+from numpy import loadtxt
+from tensorflow._api.v1.keras.models import load_model
+
+model = load_model('model_1_20.h5')
+# summarize model.
+model.summary()
+# load dataset
+dataset = loadtxt("pima-indians-diabetes.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+# evaluate the model
+score = model.evaluate(X, Y, verbose=0)
+print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
