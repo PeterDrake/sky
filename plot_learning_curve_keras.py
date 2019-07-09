dbbc0cd24d1daeacca27f8140819b8dcda3a4551
Leon Rogge <leonrogge@lclark.edu>
2019-07-09 14:19:20 -0700

    changed code to use fit_generator to attempt to make accuracy graphs

--- plot_learning_curve_keras.py
+++ plot_learning_curve_keras.py
@@ -1,14 +1,31 @@
-from numpy import loadtxt
 from tensorflow._api.v1.keras.models import load_model
+import matplotlib.pyplot as plt
+from train_model_1 import *
 
 model = load_model('model_1_20.h5')
 # summarize model.
 model.summary()
 # load dataset
-dataset = loadtxt("pima-indians-diabetes.csv", delimiter=",")
-# split into input (X) and output (Y) variables
-X = dataset[:,0:8]
-Y = dataset[:,8]
-# evaluate the model
-score = model.evaluate(X, Y, verbose=0)
-print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
+history = model.fit_generator(generator=training_batch_generator,
+						steps_per_epoch=len(train_stamps) // TRAINING_BATCH_SIZE, epochs=2, verbose=1,
+						validation_data=validation_batch_generator,
+						validation_steps=len(valid_stamps) // TRAINING_BATCH_SIZE,
+						use_multiprocessing=False, callbacks=[cb_1, tensorboard])
+
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
