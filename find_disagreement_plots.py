d51ec104fa018566dd4ff30f7fe4f83e6bc546d3
Leon Rogge <leonrogge@lclark.edu>
2019-07-11 11:04:36 -0700

    making plots for presentation, good agreement and pixel differences

--- find_disagreement_plots.py
+++ find_disagreement_plots.py
@@ -119,21 +119,27 @@ typical_agree_df['cf_diff'] = (typical_agree_df['net_fsc_z'] - typical_agree_df[
 dubious_agree_df['cf_diff'] = (dubious_agree_df['net_fsc_z'] - dubious_agree_df['fsc_z']) ** 2
 
 # Sort by column label agreement
-typical_agree_df = typical_agree_df.sort_values(by='agreement')
-dubious_agree_df = dubious_agree_df.sort_values(by='agreement')
+# typical_agree_df = typical_agree_df.sort_values(by='agreement')
+# dubious_agree_df = dubious_agree_df.sort_values(by='agreement')
 
 # Takes a selected amount of timestamps determined by NUMBER_DISAGREEMENT_IMGS and creates plots
-make_plots(typical_agree_df, TYPICAL_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/typical_agreement', 1, NUMBER_DISAGREEMENT_IMGS)
-make_plots(dubious_agree_df, DUBIOUS_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/dubious_agreement', 1, NUMBER_DISAGREEMENT_IMGS)
+# make_plots(typical_agree_df, TYPICAL_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/typical_agreement', 1, NUMBER_DISAGREEMENT_IMGS)
+# make_plots(dubious_agree_df, DUBIOUS_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/dubious_agreement', 1, NUMBER_DISAGREEMENT_IMGS)
+
+typical_agree_df = typical_agree_df.sort_values(by='agreement', ascending=False)
+make_plots(typical_agree_df, TYPICAL_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/typical_agreement/good', 1, NUMBER_DISAGREEMENT_IMGS)
 
 print(typical_agree_df)
 
 # Sort by column label cf_diff
-typical_agree_df = typical_agree_df.sort_values(by='cf_diff', ascending=False)
-dubious_agree_df = dubious_agree_df.sort_values(by='cf_diff', ascending=False)
+# typical_agree_df = typical_agree_df.sort_values(by='cf_diff', ascending=False)
+# dubious_agree_df = dubious_agree_df.sort_values(by='cf_diff', ascending=False)
 
 # Takes a selected amount of timestamps determined by NUMBER_DISAGREEMENT_IMGS and creates plots
-make_plots(typical_agree_df, TYPICAL_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/typical_cf_diff', 1, NUMBER_DISAGREEMENT_IMGS)
-make_plots(dubious_agree_df, DUBIOUS_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/dubious_cf_diff', 1, NUMBER_DISAGREEMENT_IMGS)
+# make_plots(typical_agree_df, TYPICAL_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/typical_cf_diff', 1, NUMBER_DISAGREEMENT_IMGS)
+# make_plots(dubious_agree_df, DUBIOUS_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/dubious_cf_diff', 1, NUMBER_DISAGREEMENT_IMGS)
+
+typical_agree_df = typical_agree_df.sort_values(by='cf_diff')
+make_plots(typical_agree_df, TYPICAL_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/typical_cf_diff/good', 1, NUMBER_DISAGREEMENT_IMGS)
 
 print(typical_agree_df)
