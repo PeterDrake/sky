3adcfae90458f2458a53334c615253978b69f9cf
Leon Rogge <leonrogge@lclark.edu>
2019-07-15 11:41:04 -0700

    changed print statement

--- find_disagreement_plots.py
+++ find_disagreement_plots.py
@@ -151,13 +151,9 @@ dubious_agree_df = dubious_agree_df.loc[dubious_agree_df['timestamp_utc'] == 201
 make_plots(dubious_agree_df, TYPICAL_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/timestamp', 1, 1)
 typical_agree_df = typical_agree_df.loc[typical_agree_df['timestamp_utc'] == 20130701202830]
 make_plots(typical_agree_df, TYPICAL_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/timestamp', 1, 1)
-print('hello_1')
-# if dubious_agree_df.loc[dubious_agree_df['timestamp_utc'] == 20130829002600]:
-# 	print('111')
-if typical_agree_df.loc[typical_agree_df['timestamp_utc'] == 20130701202830]:
-	print('222')
-else:
-	print('333')
+
+print(typical_agree_df.loc[typical_agree_df['timestamp_utc'] == 20130701202830])
+print(typical_agree_df.loc[typical_agree_df['timestamp_utc'] == 20130829002600])
 dubious_agree_df = dubious_agree_df.loc[dubious_agree_df['timestamp_utc'] == 20130829002600]
 make_plots(dubious_agree_df, TYPICAL_DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_LABEL + '/disagreement_figs/timestamp', 1, 1)
 typical_agree_df = typical_agree_df.loc[typical_agree_df['timestamp_utc'] == 20130829002600]
