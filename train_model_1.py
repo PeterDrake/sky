1592f26a3e8ebbe792f948db04cd8d3305fa3d28
Leon Rogge <leonrogge@lclark.edu>
2019-07-17 16:06:18 -0700

    trying to print out logs, batch and val_acc

--- train_model_1.py
+++ train_model_1.py
@@ -144,8 +144,9 @@ if __name__ == '__main__':
 
 	json_log = open('loss_log.json', mode='wt', buffering=1)
 	json_logging_callback = LambdaCallback(
+		on_batch_begin=lambda batch, logs: print(batch, logs),
 		on_batch_end=lambda epoch, logs: json_log.write(
-			json.dumps({'epoch': float(epoch), 'loss': float(logs['loss'])}) + '\n'),
+			json.dumps({'epoch': float(epoch), 'loss': float(logs['loss']), 'acc': float(logs['acc']), 'val_acc': float(logs['val_acc'])}) + '\n'),
 		on_train_end=lambda logs: json_log.close()
 	)
 
