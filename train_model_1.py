1c7d0c637bf7f89d26b8f35dc015a139d7171b32
Leon Rogge <leonrogge@lclark.edu>
2019-07-18 11:31:29 -0700

    attempting a different version of NBatchLogger

--- train_model_1.py
+++ train_model_1.py
@@ -88,19 +88,31 @@ class LossHistory(Callback):
 
 
 class NBatchLogger(Callback):
-	"""
-	A Logger that log average performance per `display` steps.
-	"""
+    """
+    A Logger that log average performance per `display` steps.
+    """
+    def __init__(self, display):
+        self.step = 0
+        self.display = display
+        self.metric_cache = {}
 
-	def __init__(self, display):
-		self.seen = 0
-		self.display = display
-
-	def on_batch_end(self, batch, logs={}):
-		self.seen += logs.get('size', 0)
-		if self.seen % self.display == 0:
-			# you can access loss, accuracy in self.params['metrics']
-			print('\n{}/{} - loss ....\n'.format(self.seen, self.params['nb_sample']))
+    def on_batch_end(self, batch, logs={}):
+        self.step += 1
+        for k in self.params['metrics']:
+            if k in logs:
+                self.metric_cache[k] = self.metric_cache.get(k, 0) + logs[k]
+        if self.step % self.display == 0:
+            metrics_log = ''
+            for (k, v) in self.metric_cache.items():
+                val = v / self.display
+                if abs(val) > 1e-3:
+                    metrics_log += ' - %s: %.4f' % (k, val)
+                else:
+                    metrics_log += ' - %s: %.4e' % (k, val)
+            print('step: {}/{} ... {}'.format(self.step,
+                                          self.params['steps'],
+                                          metrics_log))
+            self.metric_cache.clear()
 
 
 		# def corrected_accuracy(y_true, y_pred):
