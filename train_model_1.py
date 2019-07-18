d158fd8b3e3c77c621529a1987660ea78adfb8f0
Leon Rogge <leonrogge@lclark.edu>
2019-07-18 10:53:40 -0700

    set verbose = 0  and added another custom callback to print logs

--- train_model_1.py
+++ train_model_1.py
@@ -87,6 +87,35 @@ class LossHistory(Callback):
 		print(self.val_losses)
 
 
+class NBatchLogger(Callback):
+	"""
+	A Logger that log average performance per `display` steps.
+	"""
+
+	def __init__(self, display):
+		self.step = 0
+		self.display = display
+		self.metric_cache = {}
+
+	def on_batch_end(self, batch, logs={}):
+		self.step += 1
+		for k in self.params['metrics']:
+			if k in logs:
+				self.metric_cache[k] = self.metric_cache.get(k, 0) + logs[k]
+		if self.step % self.display == 0:
+			metrics_log = ''
+			for (k, v) in self.metric_cache.items():
+				val = v / self.display
+				if abs(val) > 1e-3:
+					metrics_log += ' - %s: %.4f' % (k, val)
+				else:
+					metrics_log += ' - %s: %.4e' % (k, val)
+			print('step: {}/{} ... {}'.format(self.step,
+											  self.params['steps'],
+											  metrics_log))
+			self.metric_cache.clear()
+
+
 # def corrected_accuracy(y_true, y_pred):
 # 	green = tf.constant([[0 for i in range(480)] for j in range(480)], dtype='float32')
 # 	mask = tf.not_equal(tf.reduce_sum(y_true, axis=-1), green)
@@ -167,10 +196,10 @@ if __name__ == '__main__':
 	)
 
 	history = model.fit_generator(generator=training_batch_generator,
-						steps_per_epoch=len(train_stamps) // TRAINING_BATCH_SIZE, epochs=2, verbose=1,
+						steps_per_epoch=len(train_stamps) // TRAINING_BATCH_SIZE, epochs=2, verbose=0,
 						validation_data=validation_batch_generator,
 						validation_steps=len(valid_stamps) // TRAINING_BATCH_SIZE,
-						use_multiprocessing=False, callbacks=[cb_1, tensorboard, json_logging_callback, LossHistory()])
+						use_multiprocessing=False, callbacks=[cb_1, tensorboard, json_logging_callback, NBatchLogger()])
 	# print(history.history)
 	#
 	# train_history = {}
