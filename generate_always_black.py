7e14b20d910c60cf313a951df0833a184da524f7
E00S <leonrogge@lclark.edu>
2019-10-29 22:43:07 -0700

    Deleted inessential files
    All deleted files exsit on the Old_Keras branch.

--- generate_always_black.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-from config import *
-from preprocess_stamps_launch import *
-
-
-create_constant_mask(BLACK, TYPICAL_DATA_DIR + '/always_black_mask.png', TYPICAL_DATA_DIR + '/simplemask/')
-
-
