c785d421c0eb601f3fc215181849647d058e18cf
E00S <leonrogge@lclark.edu>
2019-11-17 22:59:55 -0800

    Update u_net.py

--- u_net.py
+++ u_net.py
@@ -41,49 +41,50 @@ def build_model():
 	sky_images = Input(shape=(480, 480, 3), name='SkyImages')  # sky images
 
 	# Main body of the network
-	conv1 = Conv2D(64, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(sky_images)
-	conv1 = Conv2D(64, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv1)
+	conv1 = Conv2D(60, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(sky_images)
+	conv1 = Conv2D(60, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv1)
 	pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-	conv2 = Conv2D(128, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(pool1)
-	conv2 = Conv2D(128, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv2)
+	conv2 = Conv2D(120, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(pool1)
+	conv2 = Conv2D(120, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv2)
 	pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-	conv3 = Conv2D(256, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(pool2)
-	conv3 = Conv2D(256, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv3)
+	conv3 = Conv2D(240, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(pool2)
+	conv3 = Conv2D(240, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv3)
 	pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-	conv4 = Conv2D(512, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(pool3)
-	conv4 = Conv2D(512, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv4)
+	conv4 = Conv2D(480, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(pool3)
+	conv4 = Conv2D(480, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv4)
 	drop4 = Dropout(0.5)(conv4)
 	pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-	conv5 = Conv2D(1024, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(pool4)
-	conv5 = Conv2D(1024, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv5)
+	conv5 = Conv2D(960, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(pool4)
+	conv5 = Conv2D(960, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv5)
 	drop5 = Dropout(0.5)(conv5)
 
-	up6 = Conv2D(512, 2, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(
+	up6 = Conv2D(480, 2, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(
 		UpSampling2D(size=(2, 2))(drop5))
 	merge6 = concatenate([drop4, up6], axis=3)
-	conv6 = Conv2D(512, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(merge6)
-	conv6 = Conv2D(512, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv6)
+	conv6 = Conv2D(480, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(merge6)
+	conv6 = Conv2D(480, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv6)
 
-	up7 = Conv2D(256, 2, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(
+	up7 = Conv2D(240, 2, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(
 		UpSampling2D(size=(2, 2))(conv6))
 	merge7 = concatenate([conv3, up7], axis=3)
-	conv7 = Conv2D(256, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(merge7)
-	conv7 = Conv2D(256, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv7)
+	conv7 = Conv2D(240, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(merge7)
+	conv7 = Conv2D(240, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv7)
 
-	up8 = Conv2D(128, 2, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(
+	up8 = Conv2D(120, 2, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(
 		UpSampling2D(size=(2, 2))(conv7))
 	merge8 = concatenate([conv2, up8], axis=3)
-	conv8 = Conv2D(128, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(merge8)
-	conv8 = Conv2D(128, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv8)
+	conv8 = Conv2D(120, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(merge8)
+	conv8 = Conv2D(120, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv8)
 
-	up9 = Conv2D(64, 2, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(
+	up9 = Conv2D(60, 2, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(
 		UpSampling2D(size=(2, 2))(conv8))
 	merge9 = concatenate([conv1, up9], axis=3)
-	conv9 = Conv2D(64, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(merge9)
-	conv9 = Conv2D(64, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv9)
+	conv9 = Conv2D(60, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(merge9)
+	conv9 = Conv2D(60, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv9)
 	conv9 = Conv2D(2, 3, activation='relu', padding='same', data_format='channels_last', kernel_initializer='he_normal')(conv9)
-	conv10 = Conv2D(filters=4, kernel_size=1, padding='same', data_format='channels_last', activation='relu')(conv9)
+	concat = concatenate([conv9, sky_images], axis=3)
+	conv10 = Conv2D(filters=4, kernel_size=1, padding='same', data_format='channels_last', activation='relu')(concat)
 
 	decision = DecidePixelColors()(conv10)
 
