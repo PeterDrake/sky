709082531a0e4e4bb5c50190e8007afc963671d1
Leon Rogge <37003958+E00S@users.noreply.github.com>
2020-02-15 18:11:20 -0800

    made launch file for make_decision_images

---
+++ make_decision_images_launch.py
@@ -0,0 +1,23 @@
+import os
+from config import *
+from make_decision_images import process
+
+
+def launch_process(input_data_csv, job_name, input_dir):
+	"""Starts processing tasks."""
+
+	name = job_name + EXPERIMENT_LABEL + '-{:0>2}'.format(i)
+
+	if BLT:
+		os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 -u make_decision_images.py {} {}" -P 1'.format(name, input_dir, input_data_csv))
+	else:
+		process(int(start), int(finish), input_dir, input_data_csv)
+
+
+if __name__ == "__main__":
+
+	custom = {'DecidePixelColors': DecidePixelColors}
+	model = tf._api.v1.keras.models.load_model('model_1_23.h5', custom_objects=custom)
+
+	launch_process(DUBIOUS_DATA_CSV, "process-dubious-", DUBIOUS_DATA_DIR)
+	launch_process(TYPICAL_DATA_CSV, "process-typical-", TYPICAL_DATA_DIR)
--- new_process.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-	launch_process(DUBIOUS_DATA_CSV, "process-dubious-", DUBIOUS_DATA_DIR)
-	launch_process(TYPICAL_DATA_CSV, "process-typical-", TYPICAL_DATA_DIR)
