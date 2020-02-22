158c6cf55f5fce54f19dc188a265e50f8aa26763
Leon Rogge <37003958+E00S@users.noreply.github.com>
2020-02-22 13:55:41 -0800

    Update make_decision_images_launch.py

--- make_decision_images_launch.py
+++ make_decision_images_launch.py
@@ -9,7 +9,7 @@ def launch_process(job_name, input_dir, input_file_path):
 	name = job_name + EXPERIMENT_LABEL
 
 	if BLT:
-		os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 -u make_decision_images.py {} {} {}" -P 1'.format(name, input_dir, input_file_path))
+		os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 -u make_decision_images.py {} {}" -P 1'.format(name, input_dir, input_file_path))
 	else:
 		print("ERROR: CODE NOT WRITTEN YET. BLT = TRUE")	#TODO make this into a functioning statment that runs process.py on local machine	
 
