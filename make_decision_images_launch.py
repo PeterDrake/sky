ded942b1198c6242a7decd9a086d28d3d40326b1
Leon Rogge <37003958+E00S@users.noreply.github.com>
2020-03-08 18:51:28 -0700

    Update make_decision_images_launch.py
    Changed BLT job prio size to 40 from 12

--- make_decision_images_launch.py
+++ make_decision_images_launch.py
@@ -11,9 +11,10 @@ def launch_process(job_name, input_dir, input_file_path):
 
 	if BLT:
 		if GPU:
-			os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 -u make_decision_images.py {} {}" -P 12'.format(name, input_dir, input_file_path))
+			os.system('SGE_Batch -q gpu.q -r "{}" -c "python3 -u make_decision_images.py {} {}" -P 40'.format(name, input_dir, input_file_path))
 		else:
-			os.system('SGE_Batch -r "{}" -c "python3 -u make_decision_images.py {} {}" -P 12'.format(name, input_dir, input_file_path))
+			# THIS SHOULD'NT WORK WITH GPU TENSOR FLOW VERSION
+			os.system('SGE_Batch -r "{}" -c "python3 -u make_decision_images.py {} {}" -P 40'.format(name, input_dir, input_file_path))
 		
 	else:
 		print("ERROR: CODE NOT WRITTEN YET. BLT = TRUE")	#TODO make this into a functioning statment that runs process.py on local machine	
