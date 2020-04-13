5e790103e5e7c62a0205ae40a55d1735c1378197
Leon Rogge <37003958+E00S@users.noreply.github.com>
2020-04-13 00:42:54 -0600

    Update testing_process.py

--- testing_process.py
+++ testing_process.py
@@ -1,4 +1,4 @@
-from keras.utils import to_categorical, CustomObjectScope
+# from keras.utils import to_categorical, CustomObjectScope
 from keras.initializers import glorot_uniform
 from tensorflow.python.keras.utils.data_utils import Sequence
 # from keras.callbacks import EarlyStopping, ModelCheckpoint
