ad53f0f6583b659c1226001edd45250e4a4381bc
Peter Drake <drake@lclark.edu>
2020-12-22 16:41:53 -0800

    Can crop mask.

--- src/utils_image.py
+++ src/utils_image.py
@@ -2,6 +2,8 @@
 Utilities for dealing with images.
 """
 
+import numpy as np
+
 
 def circle_edges(mask):
     """
@@ -21,4 +23,21 @@ def center_and_radius(mask):
     top, bottom, left, right = circle_edges(mask)
     vertical_radius = (bottom - top) / 2
     horizontal_radius = (right - left) / 2
-    return ((top + bottom) / 2, (left + right) / 2), (vertical_radius + horizontal_radius) / 2
+    return (int((top + bottom) / 2),
+            int((left + right) / 2)),\
+           (vertical_radius + horizontal_radius) / 2
+
+
+def crop_mask(mask, center_and_radius):
+    """
+    Returns a version of mask cropped to 480x480, centered on the specified center.
+    :param center_and_radius: (r, c), radius, as returned by center_and_radius
+    """
+    (r, c), _ = center_and_radius
+    # Pad with zeroes (black) in case circle spills out of 480x480 region
+    padded = np.pad(mask, ((100, 100), (100, 100), (0, 0)))
+    return padded[r-140:r+340, c-140:c+340]
+
+
+def crop_photo_and_add_border(photo, center_and_radius):
+    pass
--- test/test_utils_image.py
+++ test/test_utils_image.py
@@ -6,23 +6,51 @@ import matplotlib.pyplot as plt
 
 class TestUtilsImage(unittest.TestCase):
 
-    def setUp(self):
-        circle = np.fromfunction(lambda r, c, _: (r-300)**2 + (c-200)**2 <= 100**2, (640, 480, 3))
+    def circle(self, r_center, c_center, radius):
+        """
+        Returns a mask with a circle centered at r, c with the specified radius.
+        """
+        circle = np.fromfunction(lambda r, c, _: (r - r_center) ** 2 + (c - c_center) ** 2 <= radius ** 2, (640, 480, 3))
         blue = np.full((640, 480, 3), (0, 0, 255))  # TODO Colors and standard shape should be constants
         black = np.full((640, 480, 3), (0, 0, 0))
-        self.tiny = np.where(circle, blue, black)
-        # To see the tiny image, uncomment these lines
-        # plt.imshow(self.tiny)
-        # plt.show()
+        return np.where(circle, blue, black)
 
     def test_finds_circle_edges(self):
-        self.assertEqual((200, 400, 100, 300), circle_edges(self.tiny))
+        tiny = self.circle(300, 200, 100)
+        self.assertEqual((200, 400, 100, 300), circle_edges(tiny))
 
     def test_finds_center_and_radius(self):
-        (r, c), radius = center_and_radius(self.tiny)
+        tiny = self.circle(300, 200, 100)
+        (r, c), radius = center_and_radius(tiny)
         self.assertEqual(300, r)
         self.assertEqual(200, c)
         self.assertEqual(100, radius)
+
+    def test_crops_mask(self):
+        tiny = self.circle(300, 200, 100)
+        coords = center_and_radius(tiny)
+        cropped = crop_mask(tiny, coords)
+        # To see the image, uncomment these lines
+        # plt.imshow(cropped)
+        # plt.show()
+        self.assertEqual((480, 480, 3), cropped.shape)
+        (r, c), radius = center_and_radius(cropped)
+        self.assertEqual(240, r)
+        self.assertEqual(240, c)
+        self.assertEqual(100, radius)
+
+    def test_crops_offset_circle(self):
+        tiny = self.circle(150, 300, 149)
+        coords = center_and_radius(tiny)
+        cropped = crop_mask(tiny, coords)
+        # To see the image, uncomment these lines
+        # plt.imshow(cropped)
+        # plt.show()
+        self.assertEqual((480, 480, 3), cropped.shape)
+        (r, c), radius = center_and_radius(cropped)
+        self.assertEqual(240, r)
+        self.assertEqual(240, c)
+        self.assertEqual(149, radius)
 
 
 if __name__ == '__main__':
