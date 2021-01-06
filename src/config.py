ad220b76edb48b3fbc71d1e5df6fe22b7b62fca6
Peter Drake <drake@lclark.edu>
2021-01-06 15:18:58 -0800

    Directories on BLT now specified in config.py.

--- src/config.py
+++ src/config.py
@@ -1,1 +1,5 @@
-RAW_DATA_DIR = "/home/users/jkleiss/TSI_C1"
+# Directories on BLT used by various run_*.py programs
+
+RAW_DATA_DIR = '/home/users/jkleiss/TSI_C1'
+RAW_CSV_DIR = '../raw_csv'
+DATA_DIR = '../data'
