1d11a60402513f8b49f9582e4c0e652fd054de5c
Peter Drake <drake@lclark.edu>
2021-01-27 12:33:02 -0800

    Added a TODO about creating a constant.

--- src/utils_image.py
+++ src/utils_image.py
@@ -124,6 +124,7 @@ def rgb_to_one_hot_mask(mask):
     black. These four numbers therefore provide a one-hot representation of the category of each pixel.
     """
     mask_sums = mask.sum(axis=2)  # Sum of RGB values for each pixel; each of our colors happens to have a unique sum
+    # TODO This should be a constant
     color_sums = np.array([WHITE.sum(), BLUE.sum(), GRAY.sum(), BLACK.sum()])
     # The slice [..., None] adds a third dimension, going from 480x480 to 480x480x1. This makes it possible to do a
     # broadcast comparison to color_sums, giving 4 boolean values at each pixel, making the result 480x480x4.
