15d16a6b75d0641e314cc674c8a28d620d14a6db
Peter Drake <drake@lclark.edu>
2021-02-03 10:56:17 -0800

    Made rgb_to_one_hot_mask clearer.

--- src/utils_image.py
+++ src/utils_image.py
@@ -126,9 +126,9 @@ def rgb_to_one_hot_mask(mask):
     mask_sums = mask.sum(axis=2)  # Sum of RGB values for each pixel; each of our colors happens to have a unique sum
     # TODO This should be a constant
     color_sums = np.array([WHITE.sum(), BLUE.sum(), GRAY.sum(), BLACK.sum()])
-    # The slice [..., None] adds a third dimension, going from 480x480 to 480x480x1. This makes it possible to do a
-    # broadcast comparison to color_sums, giving 4 boolean values at each pixel, making the result 480x480x4.
-    return (mask_sums[..., None] == color_sums).astype(int)
+    # The slice [:, :, np.newaxis] adds a third dimension, going from 480x480 to 480x480x1. This makes it possible to
+    # do a broadcast comparison to color_sums, giving 4 boolean values at each pixel, making the result 480x480x4.
+    return (mask_sums[:, :, np.newaxis] == color_sums).astype(int)
     # TODO Is int the type we want? uint8 will take less space, but maybe we want whatever comes out of the network
 
 
