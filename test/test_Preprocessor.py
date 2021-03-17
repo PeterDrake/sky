ebedb442d97e06cb4f35daa423be0bcf86b153a7
Peter Drake <drake@lclark.edu>
2021-03-17 12:30:11 -0700

    Fixed tests to avoid expensive preprocessing by default.

--- test/test_Preprocessor.py
+++ test/test_Preprocessor.py
@@ -8,6 +8,8 @@ from Preprocessor import *
 class TestPreprocessor(unittest.TestCase):
 
     def setUp(self):
+        print('WARNING: Some of these tests only verify that the results of the preprocessing exist.')
+        print('For a more thorough test, delete your test_data directory before running them.')
         self.preprocessor = Preprocessor('../test_raw_data', '../test_raw_csv', '../test_data', verbose=True)
 
     def test_finds_raw_photo_path(self):
@@ -46,24 +48,20 @@ class TestPreprocessor(unittest.TestCase):
 
     def test_finds_correct_numbers_of_valid_and_invalid_timestamps(self):
         self.preprocessor.validate_csv('tiny_data.csv')
-        self.assertEqual(2, self.preprocessor.valid_timestamp_count)
+        self.assertEqual(304, self.preprocessor.valid_timestamp_count)
         self.assertEqual(2, self.preprocessor.invalid_timestamp_count)
 
     def test_writes_clean_csv(self):
         self.preprocessor.write_clean_csv('tiny_data.csv')
-        data = pd.read_csv('../test_data/tiny_fake_dubious_data.csv')
+        data = pd.read_csv('../test_data/tiny_data.csv')
         # For this file, the clean version only has one valid timestamp
-        self.assertEqual(2, len(data))
+        self.assertEqual(304, len(data))
 
     def test_creates_image_directories(self):
         # Ensure that the clean CSV file exists
         self.preprocessor.write_clean_csv('tiny_data.csv')
         dirs = [self.preprocessor.data_dir + '/' + date for date in
                 ['photos/20180418', 'photos/20180419', 'tsi_masks/20180418', 'tsi_masks/20180419']]
-        # Destroy the existing directories
-        for d in dirs:
-            if os.path.isdir(d):
-                shutil.rmtree(d)
         # Run the method
         self.preprocessor.create_image_directories('tiny_data.csv')
         # Assert that the directories are present
@@ -73,7 +71,11 @@ class TestPreprocessor(unittest.TestCase):
     def test_preprocesses_files(self):
         # Ensure that the clean CSV file exists
         self.preprocessor.write_clean_csv('tiny_data.csv')
-        self.preprocessor.preprocess_images('tiny_data.csv')
+        # Only do the expensive preprocessing if it hasn't already been done
+        if not os.path.exists(self.preprocessor.data_dir + '/photos/20180418/20180418000200_photo.jpg'):
+            print('Doing expensive preprocessing of test files')
+            self.preprocessor.preprocess_images('tiny_data.csv')
+        # Now check if some of the files exist
         files = [self.preprocessor.data_dir + '/photos/20180418/20180418000200_photo.jpg',
                  self.preprocessor.data_dir + '/photos/20180419/20180419000200_photo.jpg',
                  self.preprocessor.data_dir + '/tsi_masks/20180418/20180418000200_tsi_mask.png',
