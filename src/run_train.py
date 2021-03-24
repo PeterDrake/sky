69bd757a9fe6e68114ee9c0b4ad2c4fa3fe964cc
Peter Drake <drake@lclark.edu>
2021-03-24 12:24:09 -0700

    We ran a simple network all the way through!

--- src/run_train.py
+++ src/run_train.py
@@ -3,6 +3,7 @@ from tensorflow.keras import layers
 from tensorflow import keras
 import numpy as np
 import pandas as pd
+from BatchGenerator import *
 
 # Create the network
 inputs = keras.Input(shape=(480, 480, 3))
@@ -17,4 +18,18 @@ stamps = stamps['timestamp_utc'].tolist()
 train_stamps = stamps[:96]
 val_stamps = stamps[96:192]
 
-print(train_stamps)
+train_gen = BatchGenerator(train_stamps, '../test_data')
+val_gen = BatchGenerator(val_stamps, '../test_data')
+
+# Configure the model for training.
+# We use the "sparse" version of categorical_crossentropy
+# because our target data is integers.
+model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
+
+# callbacks = [
+#     keras.callbacks.ModelCheckpoint("oxford_segmentation.h5", save_best_only=True)
+# ]
+
+# Train the model, doing validation at the end of each epoch.
+epochs = 3
+model.fit(train_gen, epochs=epochs, validation_data=val_gen)
