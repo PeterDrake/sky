b6d4c1a1ef7b81dc503bac3db7674b3b774c912f
Peter Drake <drake@lclark.edu>
2021-04-21 12:04:51 -0700

    We can process images and view them manually.

--- src/run_process.py
+++ src/run_process.py
@@ -1,4 +1,6 @@
 from BatchGenerator import *
+from ExperimentLogUpdater import ExperimentLogUpdater
+from config import *
 
 # TODO This repeats code from run_train
 stamps = pd.read_csv('../test_data/tiny_data.csv', converters={'timestamp_utc': str}, usecols=['timestamp_utc'])
@@ -6,15 +8,29 @@ stamps = stamps['timestamp_utc'].tolist()
 train_stamps = stamps[:96]
 val_stamps = stamps[96:192]
 
-model = keras.models.load_model('network.h5')
+# Load the trained model
+# TODO Change False to True to insist on a clean git state
+log_updater = ExperimentLogUpdater(RESULTS_DIR, EXPERIMENT_NAME, False)
+model = keras.models.load_model(log_updater.experiment_dir + '/network.h5')
 
-val_gen = BatchGenerator(val_stamps, '../test_data')
+# Create generator for validation data
+# TODO We'll eventually want to put testing data in here, too
+val_gen = BatchGenerator(val_stamps, '../test_data')  # TODO This should be DATA_DIR, not test_data
+
+# Produce network masks
 val_preds = model.predict(val_gen)
+# TODO Should we find a way to do this without loading ALL images into memory?
+# TODO What about parallelism?
 
+# Display one
 def display_mask(i):
     """Quick utility to display a model's prediction."""
-    mask = np.argmax(val_preds[i], axis=-1)
-    mask = np.expand_dims(mask, axis=-1)
-    plt.imshow(mask)
+    timestamp = val_stamps[i]
+    network_mask = one_hot_to_rgb_mask(val_preds[i])
+    fig, ax = plt.subplots(1, 2)
+    tsi_mask = plt.imread('../test_data' + '/tsi_masks/' + yyyymmdd(timestamp) + '/' + timestamp + '_tsi_mask.png')
+    fig.suptitle(timestamp)
+    ax[0].imshow(tsi_mask)
+    ax[1].imshow(network_mask)
 
-display_mask(0)
+display_mask(2)
--- src/run_train.py
+++ src/run_train.py
@@ -28,8 +28,8 @@ train_stamps = stamps[:96]
 val_stamps = stamps[96:192]
 
 # Create generators for training and validation data
-train_gen = BatchGenerator(train_stamps, '../test_data')
-val_gen = BatchGenerator(val_stamps, '../test_data')
+train_gen = BatchGenerator(train_stamps, '../test_data')  # TODO This should be DATA_DIR, not test_data
+val_gen = BatchGenerator(val_stamps, '../test_data')  # TODO Same here
 
 # Compile the model
 model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
@@ -42,19 +42,3 @@ callbacks = [
 
 # Train the model, doing validation at the end of each epoch
 model.fit(train_gen, epochs=TRAIN_EPOCHS, validation_data=val_gen, callbacks=callbacks)
-
-
-# # Generate predictions for all images in the validation set
-#
-# val_gen = BatchGenerator(val_stamps, '../test_data')
-# val_preds = model.predict(val_gen)
-#
-#
-# def display_mask(i):
-#     """Quick utility to display a model's prediction."""
-#     mask = np.argmax(val_preds[i], axis=-1)
-#     mask = np.expand_dims(mask, axis=-1)
-#     plt.imshow(mask)
-#     plt.colorbar()
-#
-# display_mask(0)
