2744d3822838e8b4daaa6e004f0e60208e8d7e56
Peter Drake <drake@lclark.edu>
2021-06-25 07:28:14 -0700

    Turned on full preprocessing of all the images.

--- src/launch_preprocess.py
+++ src/launch_preprocess.py
@@ -1,4 +1,4 @@
 import os
 
 os.system('SGE_Batch -r "dubious_preprocessing" -c "python3 -u ../src/run_preprocess.py shcu_dubious_data.csv"')
-# os.system('SGE_Batch -r "typical_preprocessing" -c "python3 -u ../src/run_preprocess.py shcu_typical_data.csv"')
+os.system('SGE_Batch -r "typical_preprocessing" -c "python3 -u ../src/run_preprocess.py shcu_typical_data.csv"')
