dc88cacae38552192fd049f87a8f8417507998e2
Jessica <jkleiss@lclark.edu>
2021-08-09 11:32:28 -0500

    Changed name of BLT output files to underscore.

--- src/launch_train.py
+++ src/launch_train.py
@@ -2,5 +2,5 @@ import os
 from config import *
 
 # NOTE: Run this from the `blt_job_output` directory, which should be at the same level as src.
-os.system('SGE_Batch -r "' + EXPERIMENT_NAME + '-train" -c "python3 -u ../src/run_train.py"')
-# os.system('SGE_Batch -r "' + EXPERIMENT_NAME + '-train" -q gpu.q -c "python3 -u ../src/run_train.py"')
+os.system('SGE_Batch -r "' + EXPERIMENT_NAME + '_train" -c "python3 -u ../src/run_train.py"')
+# os.system('SGE_Batch -r "' + EXPERIMENT_NAME + '_train" -q gpu.q -c "python3 -u ../src/run_train.py"')
