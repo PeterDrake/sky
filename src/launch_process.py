6ea4c7f274a7e22e29dc25a294add050dac0251d
Peter Drake <drake@lclark.edu>
2021-08-10 09:18:42 -0700

    launch_process now processes batches in parallel

--- src/launch_process.py
+++ src/launch_process.py
@@ -4,3 +4,9 @@ from config import *
 # NOTE: Run this from the `blt_job_output` directory, which should be at the same level as src.
 os.system('SGE_Batch -r "' + EXPERIMENT_NAME + '_process" -c "python3 -u ../src/run_process.py"')
 # os.system('SGE_Batch -r "' + EXPERIMENT_NAME + '_process" -q gpu.q -c "python3 -u ../src/run_process.py"')
+
+# TODO Modify run_process to take these command-line arguments, then uncomment this
+# n = 21965  # Number of validation images
+# for i in range(0, n, 3200):
+#     j = min(i + 3200, n)
+#     os.system('SGE_Batch -r "{}_process" -c "python3 -u ../src/run_process.py {} {}"'.format(EXPERIMENT_NAME, i, j))
