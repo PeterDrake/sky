25b89f12e60ffa98004be58dac8c29d7f5084251
Jessica <jkleiss@lclark.edu>
2021-08-11 11:56:31 -0500

    We can now count the pixels in all masks in a directory.

--- src/FscCalculator.py
+++ src/FscCalculator.py
@@ -1,6 +1,15 @@
 from utils_image import *
+import pandas as pd
+from utils_timestamp import *
+import os
+from skimage.io import imread
+
 
 class FscCalculator:
+
+    def __init__(self, mask_dir):
+        self.mask_dir = mask_dir
+
 
     def count_pixels(self, mask):
         """
@@ -9,7 +18,29 @@ class FscCalculator:
         """
         return [(mask == c).all(axis=2).sum() for c in [BLUE, GRAY, WHITE]]
 
+    def timestamp_to_mask_path(self, timestamp):
+        """
+        Returns the full path for the network or TSI mask file for the given timestamp.
+        Note that self.mask_dir indicates if we desire a tsi or network mask. Use this information to obtain
+        the correct filename ('*_network_* or *_tsi_*).
+        """
+        mask_path = self.mask_dir + '/' + yyyymmdd(timestamp) + '/' + timestamp + '_network_mask.png'
+        if os.path.exists(mask_path):
+            return mask_path
+        else:
+            return self.mask_dir + '/' + yyyymmdd(timestamp) + '/' + timestamp + '_tsi_mask.png'
 
+
+    def count_pixels_in_all_masks(self, stamps):
+        """
+        Return a dataframe containing the clear/thin/opaque counts for all masks in the list of stamps.
+        """
+        result = pd.DataFrame(index=stamps, columns = ('clear_160', 'thin_160', 'opaque_160'))
+        for timestamp in stamps:
+            mask = imread(self.timestamp_to_mask_path(timestamp))[:, :, :3]
+            counts_160 = self.count_pixels(mask) # in the full mask
+            result.loc[timestamp] = counts_160
+        return result
 
 
 # 1) Create an object instance that includes directory names
--- test/test_FscCalculator.py
+++ test/test_FscCalculator.py
@@ -7,8 +7,14 @@ from FscCalculator import *
 class TestFscCalculator(unittest.TestCase):
 
     def setUp(self):
-        self.calculator = FscCalculator()
+        self.calculator = FscCalculator('../test_network_masks')
 
     def test_counts_pixels(self):
         mask = np.array([[WHITE, BLUE], [WHITE, BLACK]])
         self.assertEqual([1, 0, 2], self.calculator.count_pixels(mask))
+
+    def test_count_pixels_in_all_masks(self):
+        stamps = ['20120501170000', '20120501170030', '20120501170100', '20120501170130']
+        df = self.calculator.count_pixels_in_all_masks(stamps)
+        print(df)
+        print(df.sum(axis=1))
