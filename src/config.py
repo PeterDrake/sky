1d06f8c083de024d9657f3091d153ac35831899a
Peter Drake <drake@lclark.edu>
2022-09-29 13:30:41 -0700

    Updated some documentation for the revised BLT job system (through the first step of preprocessing).

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-# Directories on BLT used by various run_*.py programs
+# Directories used by various run_*.py programs
 RAW_DATA_DIR = '/home/users/jkleiss/TSI_C1'
 RAW_CSV_DIR = '../raw_csv'
 DATA_DIR = '/home/drake/sky/data'
