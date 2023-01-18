b44799de2ea0fb23792f2190dd852af30966f485
Peter Drake <drake@lclark.edu>
2023-01-18 11:58:03 -0800

    Added debug messages to run_collate_fsc_cf.py.

--- src/run_collate_fsc_cf.py
+++ src/run_collate_fsc_cf.py
@@ -6,8 +6,8 @@ with open(DATA_DIR + '/' + TYPICAL_TIMESTAMP_FILENAMES[1], 'r') as f:
     val_stamps = [line.strip() for line in f.readlines()]
 
 # Read the TSI FSCs
-tsi_fsc_df = pd.read_csv(DATA_DIR + '/typical_validation_tsi_fsc.csv', index_col=0)
-tsi_fsc_df.index.name = 'timestamp_utc'
+tsi_fsc_df = pd.read_csv(DATA_DIR + '/typical_validation_tsi_fsc.csv')
+# tsi_fsc_df.index.name = 'timestamp_utc'
 print(tsi_fsc_df.info())
 
 # Read the ceilometer CFs
