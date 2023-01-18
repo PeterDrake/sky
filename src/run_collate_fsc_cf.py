8e4d4063f581a869693e5c9bc036f517f1c81c21
Peter Drake <drake@lclark.edu>
2023-01-18 11:54:15 -0800

    Added debug messages to run_collate_fsc_cf.py.

--- src/run_collate_fsc_cf.py
+++ src/run_collate_fsc_cf.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from config import *
 
@@ -9,12 +8,15 @@ with open(DATA_DIR + '/' + TYPICAL_TIMESTAMP_FILENAMES[1], 'r') as f:
 # Read the TSI FSCs
 tsi_fsc_df = pd.read_csv(DATA_DIR + '/typical_validation_tsi_fsc.csv', index_col=0)
 tsi_fsc_df.index.name = 'timestamp_utc'
+print(tsi_fsc_df.info())
 
 # Read the ceilometer CFs
 cf_df = pd.read_csv(RAW_CSV_DIR + '/shcu_typical_data.csv', usecols=['timestamp_utc', 'cf_shcu'])
+print(cf_df.info())
 
 # Join the dataframes
 result = tsi_fsc_df.merge(cf_df, on='timestamp_utc', how='inner')
+print(result.info())
 
 # Export the result
 result.to_csv(RESULTS_DIR + '/' + EXPERIMENT_NAME + '/' + 'collate_fsc_cf.csv')
