fb652d221fa770a1ebbb21aa0734c5b2cd4690d0
Peter Drake <drake@lclark.edu>
2023-02-15 13:08:04 -0800

    Continuing to refince scatter.py.

--- src/run_collate_fsc_cf.py
+++ src/run_collate_fsc_cf.py
@@ -7,16 +7,11 @@ def collate_fsc_cf(category):
     '''
     # Read the TSI FSCs
     tsi_fsc_df = pd.read_csv(DATA_DIR + '/' + category + '_validation_tsi_fsc.csv')
-    # tsi_fsc_df.index.name = 'timestamp_utc'
-    print(tsi_fsc_df.info())
     # Read the ceilometer CFs
     cf_df = pd.read_csv(RAW_CSV_DIR + '/shcu_' + category + '_data.csv', usecols=['timestamp_utc', 'cf_shcu'])
-    print(cf_df.info())
     cf_df = cf_df.drop_duplicates()
-    print(cf_df.info())
     # Join the dataframes
     result = tsi_fsc_df.merge(cf_df, on='timestamp_utc', how='inner')
-    print(result.info())
     # Export the result
     result.to_csv(RESULTS_DIR + '/' + EXPERIMENT_NAME + '/' + 'collate_fsc_cf_' + category + '.csv')
 
--- src/scatter.py
+++ src/scatter.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('../plotting_data/collate_fsc_cf.csv')
+df = pd.read_csv('../plotting_data/collate_fsc_cf_dubious.csv')
 df['fsc'] = df['opaque_160'] / (df['clear_160'] + df['thin_160'] + df['opaque_160'])
 plt.scatter(df['cf_shcu'], df['fsc'], s=0.1)
-plt.title('Typical data')
+plt.title('Dubious data')
 plt.xlabel('CF')
 plt.ylabel('TSI FSC')
 plt.plot([0,1], [0, 1], color='orange')
