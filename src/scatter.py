bc9dc139f5dc22e1ed587cc761fcc41cf4e2a9f0
Peter Drake <drake@lclark.edu>
2023-02-15 11:49:09 -0800

    Refactored run_collate_fsc_cf.py to allow us to specify typical or dubious.

--- src/scatter.py
+++ src/scatter.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('../plotting_data/collate_fsc_cf.csv')
 df['fsc'] = df['opaque_160'] / (df['clear_160'] + df['thin_160'] + df['opaque_160'])
-plt.scatter(df['fsc'], df['cf_shcu'], s=0.1)
-plt.plot([0,1], [0, 1])
+plt.scatter(df['cf_shcu'], df['fsc'], s=0.1)
+plt.title('Typical data')
+plt.xlabel('CF')
+plt.ylabel('TSI FSC')
+plt.plot([0,1], [0, 1], color='orange')
 plt.show()
