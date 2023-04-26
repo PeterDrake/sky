5c6abe9c2e721e0200bc2de7e55418e57dbed33f
Peter Drake <drake@lclark.edu>
2023-04-26 12:03:40 -0700

    Can now find start time for shifting window.

--- src/WindowFinder.py
+++ src/WindowFinder.py
@@ -1,9 +1,15 @@
+from datetime import datetime, timedelta
+
 class WindowFinder:
     """
     Finds 15-minute windows of timestamps centered on each TSI image timestamp.
     """
 
+    # Set the width of the window (in minutes)
+    HALF_WIDTH = 7.5
+
     def __init__(self, timestamp_filename):
+        # It is assumed that the timestamps in the file are sorted!
         with open(timestamp_filename, 'r') as f:
             self.stamps = [line.strip() for line in f.readlines()]
 
@@ -12,3 +18,20 @@ class WindowFinder:
         Returns a list of the years that appear in this WindowFinder's list of timestamps.
         """
         return sorted(list(set(s[:4] for s in self.stamps)))
+
+    def first_timestamp(self, year):
+        """
+        Returns the first timestamp in year within this WindowFinder's list of timestamps.
+        """
+        for s in self.stamps:
+            if s.startswith(year):
+                return s
+
+    def find_begin_time(self, stamp):
+        """
+        Returns a timestamp that is before stamp by the amount specified by HALF_WIDTH.
+        """
+        dt = datetime.strptime(stamp, '%Y%m%d%H%M%S')
+        start = dt - timedelta(minutes=WindowFinder.HALF_WIDTH)
+        return start.strftime('%Y%m%d%H%M%S')
+
--- test/test_WindowFinder.py
+++ test/test_WindowFinder.py
@@ -2,10 +2,16 @@ import unittest
 from WindowFinder import *
 
 
-class TestPreprocessor(unittest.TestCase):
+class TestWindowFinder(unittest.TestCase):
 
     def setUp(self):
         self.window_finder = WindowFinder('../test_data/typical_validation_timestamps')
 
     def test_finds_years(self):
         self.assertEqual(['2012', '2013', '2014', '2015', '2016', '2017'], self.window_finder.years())
+
+    def test_finds_first_timestamp(self):
+        self.assertEqual('20150510201500', self.window_finder.first_timestamp('2015'))
+
+    def test_finds_begin_time(self):
+        self.assertEqual('20150510200730', self.window_finder.find_begin_time('20150510201500'))
