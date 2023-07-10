9a997d4301718e166b7da3856b2ee67ad845d102
Peter Drake <drake@lclark.edu>
2023-07-10 13:06:30 -0700

    Playing with plot_fsc_scatter.py

--- src/plot_fsc_scatter.py
+++ src/plot_fsc_scatter.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('../data_for_plotting/collate_tsi_fsc_cf_dubious.csv')
+# df = pd.read_csv('../data_for_plotting/collate_tsi_fsc_cf_dubious.csv')
+df = pd.read_csv('../data_for_plotting/collate_network_fsc_cf_dubious.csv')
 
 plt.scatter(df['fsc_opaque_100'], df['cf_shcu'], s=0.5, alpha=0.5)
 plt.plot([0, 1], [0, 1], color='red')
