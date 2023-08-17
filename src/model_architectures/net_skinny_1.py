c6bda0a117b1f9108d4f6ae2bdb241fee0791af0
Peter Drake <drake@lclark.edu>
2023-08-17 09:11:41 -0700

    Reduced filters from 64 to 32 in conv layer because BLT can't handle concatenating 64 with the max-pool layers.

--- src/model_architectures/net_skinny_1.py
+++ src/model_architectures/net_skinny_1.py
@@ -7,7 +7,7 @@ DefaultConv2D = partial(layers.Conv2D, kernel_size=3, activation='relu', kernel_
 DefaultPool = partial(layers.MaxPooling2D, strides=(1, 1), padding='same')
 
 inputs = keras.Input(shape=RGB_PHOTO_SIZE)
-c1 = DefaultConv2D(filters=64)(inputs)
+c1 = DefaultConv2D(filters=32)(inputs)
 p1 = DefaultPool(pool_size=(1, 100))(c1)
 p2 = DefaultPool(pool_size=(100, 1))(c1)
 cat = layers.Concatenate()((c1, p1, p2, inputs))
