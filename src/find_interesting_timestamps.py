fa8e8f73a5b1b56a18b4dc5c9a39f93221ba3a9c
Peter Drake <drake@lclark.edu>
2023-08-23 13:49:57 -0700

    Created find_interesting_timestamps.py

--- src/find_interesting_timestamps.py
+++ src/find_interesting_timestamps.py
@@ -5,7 +5,7 @@ import random
 Finds some representative timestamps for manual inspections.
 """
 
-df = pd.read_csv('../data_for_plotting/collate_network_fsc_cf_typical.csv')
+df = pd.read_csv('../data_for_plotting/collate_network_fsc_cf_dubious.csv')
 desired_cfs = (0.0, 0.25, 0.5, 0.75, 1.0)
 tolerance = 0.001
 for desired in desired_cfs:
