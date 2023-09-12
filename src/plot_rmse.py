64355eb3c421b989498aac2f69ed1a3dd225ec32
Peter Drake <drake@lclark.edu>
2023-09-12 13:45:57 -0700

    plot_rme computes the values we want

--- src/plot_rmse.py
+++ src/plot_rmse.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv('../data_for_plotting/collate_network_fsc_cf_dubious.csv')
-rmse = mean_squared_error(df['cf_shcu'], df['fsc_opaque_100'] + df['fsc_thin_100'], squared=False)
-print(rmse)
+for source in ('tsi', 'network'):
+    print(source)
+    for quality in ('typical', 'dubious'):
+        print(quality)
+        df = pd.read_csv(f'../data_for_plotting/collate_{source}_fsc_cf_{quality}.csv')
+        df.dropna(inplace=True)
+        rmse = mean_squared_error(df['cf_shcu'], df['fsc_opaque_100'] + df['fsc_thin_100'], squared=False)
+        print(rmse)
