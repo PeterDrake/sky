8bc31ae02e011f8ae505701b486b9ba717353589
Peter Drake <drake@lclark.edu>
2023-12-05 10:33:18 -0800

    Modified run_calculate_tsi_fsc.py to also calculate FSCs for training data.

--- src/run_calculate_tsi_fsc.py
+++ src/run_calculate_tsi_fsc.py
@@ -2,7 +2,8 @@ from FscCalculator import FscCalculator
 from config import *
 
 calc = FscCalculator(DATA_DIR, DATA_DIR + '/tsi_masks', DATA_DIR)
-# TODO We're only doing validation now; eventually we'll need to do this for testing data
-# The indices 1 and 0 below indicate the validation set
+# TODO We're only doing training and validation now; eventually we'll need to do this for testing data
+# The indices for the typical and dubious timestamp filename lists are different; see config.py
+calc.write_pixel_counts(TYPICAL_TIMESTAMP_FILENAMES[0], 'typical_training_tsi_fsc.csv')
 calc.write_pixel_counts(TYPICAL_TIMESTAMP_FILENAMES[1], 'typical_validation_tsi_fsc.csv')
 calc.write_pixel_counts(DUBIOUS_TIMESTAMP_FILENAMES[0], 'dubious_validation_tsi_fsc.csv')
