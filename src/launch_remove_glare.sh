18ba5060f6e8a3a5b9eb127722c11cf66be054bf
Peter Drake <drake@lclark.edu>
2024-01-09 11:47:59 -0800

    launch_remove_glare.sh should now read from the correct csv file.

--- src/launch_remove_glare.sh
+++ src/launch_remove_glare.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # NOTE: Run this from the `blt_job_output` directory, which should be at the same level as src.
-# We don't need to do this for dubious data, because we don't train on dubious data
-#python3 -u ../src/run_remove_glare.py shcu_dubious_data.csv
-python3 -u ../src/run_remove_glare.py shcu_typical_data.csv
+python3 -u ../src/run_remove_glare.py typical_training_tsi_fsc.csv
