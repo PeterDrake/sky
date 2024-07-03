4fea2c40e5d5054bb0e5c3419d415f8d7da85548
Peter Drake <drake@lclark.edu>
2024-07-03 08:12:57 -0700

    Most code is now set up to use testing in addition to or instead of validation data.

--- src/run_calculate_network_fsc.py
+++ src/run_calculate_network_fsc.py
@@ -2,6 +2,6 @@ from FscCalculator import FscCalculator
 from config import *
 
 calc = FscCalculator(DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_NAME + '/network_masks', RESULTS_DIR + '/' + EXPERIMENT_NAME)
-# TODO We're only doing validation now; eventually we'll need to do this for testing data
-calc.write_pixel_counts(TYPICAL_TIMESTAMP_FILENAMES['validation'], 'typical_validation_network_fsc.csv')
-calc.write_pixel_counts(DUBIOUS_TIMESTAMP_FILENAMES['validation'], 'dubious_validation_network_fsc.csv')
+for quality in ['typical', 'dubious']:
+    for mode in ['validation', 'testing']:
+        calc.write_pixel_counts(TYPICAL_TIMESTAMP_FILENAMES[mode], f'{quality}_{mode}_network_fsc.csv')
--- src/run_process.py
+++ src/run_process.py
@@ -4,33 +4,32 @@ from ExperimentLogUpdater import ExperimentLogUpdater
 from config import *
 from skimage.io import imsave
 
+mode = 'testing'  # 'validation' or 'testing'
+
 # Get timestamps for the data to use
-val_stamps = []
-with open(DATA_DIR + '/' + TYPICAL_TIMESTAMP_FILENAMES['validation'], 'r') as f:
+stamps = []
+with open(DATA_DIR + '/' + TYPICAL_TIMESTAMP_FILENAMES[mode], 'r') as f:
     for line in f.readlines():
-        val_stamps.append(line.strip())
-with open(DATA_DIR + '/' + DUBIOUS_TIMESTAMP_FILENAMES['validation'], 'r') as f:
+        stamps.append(line.strip())
+with open(DATA_DIR + '/' + DUBIOUS_TIMESTAMP_FILENAMES[mode], 'r') as f:
     for line in f.readlines():
-        val_stamps.append(line.strip())
+        stamps.append(line.strip())
 
 # Load the trained model
 log_updater = ExperimentLogUpdater(RESULTS_DIR, EXPERIMENT_NAME, True)
 model = keras.models.load_model(log_updater.experiment_dir + '/network.h5')
 
-# Create generator for validation data
-# TODO Right before publication, we'll eventually want to put test data (as opposed to validation data) in here
-print('Processing {} photos'.format(len(val_stamps)))
-for i in range(0, len(val_stamps), 320):
+print('Processing {} photos'.format(len(stamps)))
+for i in range(0, len(stamps), 320):
     print('Starting chunk ' + str(i) + '-' + str(i+320))
-    chunk = val_stamps[i:i+320]
+    chunk = stamps[i:i + 320]
     print(str(len(chunk)) + ' images')
-    val_gen = BatchGenerator(chunk, DATA_DIR)
+    generator = BatchGenerator(chunk, DATA_DIR)
     # Produce network masks
-    val_preds = model.predict(val_gen)
+    predictions = model.predict(generator)
     # Save the files
-    # If, for debugging purposes, we want to run this on just a few images, change val_stamps to val_stamps[:4]
     for i, timestamp in enumerate(chunk):
-        network_mask = one_hot_to_rgb_mask(val_preds[i])
+        network_mask = one_hot_to_rgb_mask(predictions[i])
         dir = log_updater.experiment_dir + '/network_masks/' + yyyymmdd(timestamp) + '/'
         os.makedirs(dir, exist_ok=True)
         imsave(timestamp_to_network_mask_path(log_updater.experiment_dir, timestamp), network_mask)
