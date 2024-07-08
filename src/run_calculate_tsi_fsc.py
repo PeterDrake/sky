39f8529bf753a9c7444f483ccedd14d7bac6d63b
Peter Drake <drake@lclark.edu>
2024-07-08 07:45:05 -0700

    Removed outdated reference to "suffix"

--- src/run_calculate_tsi_fsc.py
+++ src/run_calculate_tsi_fsc.py
@@ -1,7 +1,7 @@
 from FscCalculator import FscCalculator
 from config import *
 
-calc = FscCalculator(DATA_DIR, DATA_DIR + f'/tsi_masks{suffix}', DATA_DIR)
+calc = FscCalculator(DATA_DIR, DATA_DIR + f'/tsi_masks', DATA_DIR)
 calc.write_pixel_counts(TYPICAL_TIMESTAMP_FILENAMES['training'], f'typical_training_tsi_fsc.csv')
 for category in ['validation', 'testing']:
     calc.write_pixel_counts(TYPICAL_TIMESTAMP_FILENAMES[category], f'typical_{category}_tsi_fsc.csv')
