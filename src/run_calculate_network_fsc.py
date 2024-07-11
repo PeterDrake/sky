8182afa60436f1cecbd196c0f8cd2d0a4cfa0ae4
Peter Drake <drake@lclark.edu>
2024-07-10 22:01:42 -0700

    Debug printlns.

--- src/run_calculate_network_fsc.py
+++ src/run_calculate_network_fsc.py
@@ -3,6 +3,6 @@ from config import *
 
 calc = FscCalculator(DATA_DIR, RESULTS_DIR + '/' + EXPERIMENT_NAME + '/network_masks', RESULTS_DIR + '/' + EXPERIMENT_NAME)
 for quality in ['typical', 'dubious']:
-    print(f'{quality}: {TYPICAL_TIMESTAMP_FILENAMES[NETWORK_IMAGE_CATEGORY][:3]}')
+    print(f'{quality}: {TYPICAL_TIMESTAMP_FILENAMES[NETWORK_IMAGE_CATEGORY]} -> {quality}_{NETWORK_IMAGE_CATEGORY}_network_fsc.csv')
     calc.write_pixel_counts(TYPICAL_TIMESTAMP_FILENAMES[NETWORK_IMAGE_CATEGORY],
                             f'{quality}_{NETWORK_IMAGE_CATEGORY}_network_fsc.csv')
