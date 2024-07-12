a624d9f847e545e039e33d29eca2266126347b06
jkleiss <jkleiss@lclark.edu>
2024-07-12 11:52:11 -0700

    read_nc_shcu_files.py reads both the VAP times of ShCu, and Erin's typical and dubious times of ShCu and compares the numbers of images found.

--- src/read_nc_shcu_files.py
+++ src/read_nc_shcu_files.py
@@ -2,9 +2,14 @@ import os
 import pandas as pd
 import xarray as xr
 import numpy as np
+import matplotlib.pyplot as plt
 
-# First just read through the netcdf files and extract the day, start_hour, end_hour, and two flags.
 def read_netcdf_files(directory = '../ShCu_times'):
+    #  Read through the netcdf files and extract the
+    #  day, start_hour, end_hour, and two flags.
+    # Create a Dataframe containing (day, start_hour, end_hour, event, event_test)
+    # Then turn it into a list of Timestamps
+
     files = os.listdir(directory)
     print(files)
     # initialize output. This will be a dataframe with 5 columns containing:
@@ -29,8 +34,9 @@ def read_netcdf_files(directory = '../ShCu_times'):
             # make 'time' the same dimension as the others.
             time = np.tile(time1,(4, 1)).transpose()
 
-            # Get only the values corresponding to non-nan start_hour values
-            mask = ~np.isnan(start_hour)
+            # Get only the values corresponding to non-nan start_hour values,
+            # and shallowcumulus_event_tests == 0. (no overlying cirrus)
+            mask = (~np.isnan(start_hour)) & (shallowcumulus_event_tests == 0)
             tt = time[mask]
             sh = start_hour[mask]
             eh = end_hour[mask]
@@ -45,26 +51,58 @@ def read_netcdf_files(directory = '../ShCu_times'):
 
     # Concatenate all DataFrames into a single DataFrame
     output = pd.concat(dfs, ignore_index=True)
-    return output
 
-# def df_to_timestamp(shcu_data):
+    # turn each row into a list of times with 5M spacing, and flatten the list of lists
+    time_intervals = [generate_time_range(row['day'], row['start_hour'], row['end_hour']) for index, row in output.iterrows()]
+    shcu_times = [time for sublist in time_intervals for time in sublist]
 
-# def generate_time_range(day, start_hour, end_hour)
-    start_time = pd.Timestamp.combine(pd.Timestamp)
+    return shcu_times
 
+def generate_time_range(day, start_hour, end_hour):
+    # Create a list of datetime stamps from start_time to end_time with frequency 5 minutes.
+        start_time = day + pd.Timedelta(hours=start_hour)
+        end_time = day + pd.Timedelta(hours=end_hour)
+        return pd.date_range(start=start_time, end=end_time, freq='5T').tolist()
 
-# Create a Dataframe containing (day, start_hour, end_hour, event, event_test)
-output = read_netcdf_files()
+def read_erin_files(filename):
+    # Read Erin's times of ShCu
+    df = pd.read_csv(filename, converters={'timestamp_utc': str},
+                     usecols=['timestamp_utc','fsc_z','fsc_thn_z','cf_shcu'])
 
-#            day  start_hour  end_hour  event  event_test
-# 0   2012-05-01        18.0      23.0     32           0
-# 1   2012-05-02        15.0      22.0      1           0
-# 2   2012-05-09        20.0      24.0      1           0
-# 3   2012-05-11         1.0       2.0     16           0
+    # sub-sample to every 5M
+    mask = df['timestamp_utc'].str.endswith('000') | df['timestamp_utc'].str.endswith('500')
+    df = df[mask]
+    df['erin_times']=pd.to_datetime(df['timestamp_utc'], format= '%Y%m%d%H%M%S')
+    return df
 
-# Convert this into a list of times every 5 minutes.
-# shcu_times = df_to_timestamp()
+shcu_times = read_netcdf_files()
 
-# Get the times for our analysis
+# Now read Erin's files. Return a dataframe with timestamps and corresponding FSC & CF
+typ = read_erin_files('../raw_csv/shcu_typical_data.csv')
+dub = read_erin_files('../raw_csv/shcu_dubious_data.csv')
 
-a="hi"
+shcu_set = set(shcu_times) # len = 6063
+typ_set = set(typ['erin_times']) # len = 10376
+dub_set = set(dub['erin_times']) # len = 10376
+
+# Describe the overlap between the two.
+LT = len(typ_set)
+LD = len(dub_set)
+LS = len(shcu_set)
+both = shcu_set.intersection(typ_set.union(dub_set))
+LB = len(both)
+both_typ = typ_set.intersection(shcu_set)
+both_dub = dub_set.intersection(shcu_set)
+print('VAP finds %d images. %.0d%% are in Erin''s set' % (LS, LB/LS*100))
+print('Erin finds %d typical and %d dubious images, of which '
+      '%.0d%% and %.0d%% are in the VAP, respectively' %
+      (LT, LD, len(both_typ)/LT*100, len(both_dub)/LD*100))
+
+print("Typical images 5 min sampling. Erin: %d; VAP: %d images" % (LT, LS))
+print("Overlap: %d images (%.0d%% of Erin's, %.0d%% of VAP)" % (LB, LB/LT*100, LB/LS*100))
+
+both = shcu_set.intersection(dub_set)
+LB = len(both)
+print("Dubious images 5 min sampling. Erin: %d; VAP: %d images" % (LD, LS))
+print("Overlap: %d images (%.0d%% of Erin's, %.0d%% of VAP)" % (LB, LB/LD*100, LB/LS*100))
+
