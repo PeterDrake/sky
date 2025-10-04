4fa1781e78378efae879b0076bd479758358bc44
Peter Drake <drake@lclark.edu>
2025-10-04 10:38:27 -0700

    Trying to fix import in run_train.py.

--- src/run_train.py
+++ src/run_train.py
@@ -1,4 +1,5 @@
 import tensorflow.keras.optimizers
+import tensorflow.keras
 
 from BatchGenerator import *
 from ExperimentLogUpdater import *
@@ -29,7 +30,7 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 # Specify callbacks to use during training
 callbacks = [
     # Save the model regularly, keeping only the best one
-    keras.callbacks.ModelCheckpoint(log_updater.experiment_dir + '/network.h5', save_best_only=True)
+    tensorflow.keras.callbacks.ModelCheckpoint(log_updater.experiment_dir + '/network.h5', save_best_only=True)
 ]
 
 # Train the model, doing validation at the end of each epoch
