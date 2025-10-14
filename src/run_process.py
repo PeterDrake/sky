67e2c3196fb0f753399939d9b09931eadf1dcbff
Peter Drake <drake@lclark.edu>
2025-10-14 07:55:57 -0700

    Changed stored network file from .h5 to .keras.

--- src/run_process.py
+++ src/run_process.py
@@ -34,7 +34,7 @@ with open(DATA_DIR + '/' + DUBIOUS_TIMESTAMP_FILENAMES[NETWORK_IMAGE_CATEGORY], 
 
 # Load the trained model
 log_updater = ExperimentLogUpdater(RESULTS_DIR, EXPERIMENT_NAME, True)
-model = keras.models.load_model(log_updater.experiment_dir + '/network.h5')
+model = tf_keras.models.load_model(log_updater.experiment_dir + '/network.keras')
 
 print('Processing {} photos'.format(len(stamps)))
 for i in range(0, len(stamps), 320):
